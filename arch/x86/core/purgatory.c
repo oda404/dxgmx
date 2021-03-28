4a03807b14e9c00bcfeda7c868881eddebfd3c36
oda404 <xdxalexandru404@gmail.com>
2021-03-29 00:07:26 +0300

    Remove purgatory_enter_msg

--- arch/x86/core/purgatory.c
+++ arch/x86/core/purgatory.c
@@ -5,8 +5,3 @@ void purgatory_enter()
 {
     asm("1: cli; hlt; jmp 1");
 }
-
-void purgatory_enter_msg(const char *last_msg)
-{
-    asm("1: cli; hlt; jmp 1");
-}
--- include/dxgmx/purgatory.h
+++ include/dxgmx/purgatory.h
@@ -3,6 +3,5 @@
 #define __DXGMX_PURGATORY_H__
 
 void purgatory_enter();
-void purgatory_enter_msg(const char *last_msg);
 
 #endif // __DXGMX_PURGATORY_H__
