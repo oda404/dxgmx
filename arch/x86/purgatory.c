584c9d142603ed9fa8454bc79a4857291fd13a1e
oda404 <xdxalexandru404@gmail.com>
2021-04-07 02:31:01 +0300

    Remove purgatory

--- arch/x86/purgatory.c
+++
@@ -1,7 +0,0 @@
-
-#include<dxgmx/purgatory.h>
-
-void purgatory_enter()
-{
-    asm("1: cli; hlt; jmp 1");
-}
--- include/dxgmx/purgatory.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef __DXGMX_PURGATORY_H__
-#define __DXGMX_PURGATORY_H__
-
-void purgatory_enter();
-
-#endif // __DXGMX_PURGATORY_H__
