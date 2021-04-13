39e388b6386fc1936ee77f97dfc886d78e132d4b
oda404 <xdxalexandru404@gmail.com>
2021-04-13 18:00:26 +0300

    [include] Add multiple structs and macros to dxgmx/x86/mboot2.h

--- include/dxgmx/x86/mboot2.h
+++ include/dxgmx/x86/mboot2.h
@@ -6,11 +6,17 @@
 #define MBOOT2_HEADER_MAGIC      0xE85250D6
 #define MBOOT2_BOOTLOADER_MAGIC  0x36D76289
 
-#define MBOOT2_HEADER_TAG_END    0
-#define MBOOT2_HEADER_TAG_OPT    1
+#define MBOOT2_HEADER_TAG_END       0
+#define MBOOT2_HEADER_TAG_OPT       1
+#define MBOOT2_HEADER_TAG_FRAMEBUFF 5
 
+#define MBOOT2_TAG_END           0
+#define MBOOT2_TAG_BOOTCLI       1
 #define MBOOT2_TAG_MMAP          6
 #define MBOOT2_TAG_FRAMEBUFFER   8
+#define MBOOT2_TAG_IMG_BASE_ADDR 21
+
+#define MBOOT2_TAGS_ALIGN        8
 
 #define MBOOT2_ARCH_I386         0
 
@@ -28,7 +34,41 @@ typedef struct
 {
     uint32_t type;
     uint32_t size;
-} mboot_tag;
+} mboot2_mbi_tag_header;
+
+typedef struct
+{
+    uint32_t total_size;
+    uint32_t reserved;
+} mboot2_mbi_fixed;
+
+typedef struct
+{
+    mboot2_mbi_tag_header header;
+    uint8_t *string; /* string passed from the boot cli */
+} mboot2_mbi_tag_bootcli;
+
+typedef struct
+{
+    uint32_t size;
+    uint64_t base_addr;
+    uint64_t length;
+    uint32_t type;
+} mboot2_mmap_entry __attribute__((packed));
+
+typedef struct
+{
+    mboot2_mbi_tag_header header;
+    uint32_t entry_size;
+    uint32_t entry_version;
+    mboot2_mmap_entry *entries;
+} mboot2_mbi_tag_mmap;
+
+typedef struct
+{
+    mboot2_mbi_tag_header header;
+    uint32_t base_addr;
+} mboot2_mbi_tag_img_base_addr;
 
 #endif // __ASM__
 
