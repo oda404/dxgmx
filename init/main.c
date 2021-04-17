781633e67af218af04a05c04bb545588d2f1a130
oda404 <xdxalexandru404@gmail.com>
2021-04-17 14:24:39 +0300

    [init] Read mmap from multiboot struct

--- init/main.c
+++ init/main.c
@@ -15,7 +15,8 @@
 #include<dxgmx/abandon_ship.h>
 #include<dxgmx/gcc/attrs.h>
 #include<dxgmx/video/tty.h>
-#include<stdio.h>
+#include<dxgmx/kstdio.h>
+#include<stdint.h>
 
 #if defined(__MBOOT2__)
 
@@ -33,22 +34,46 @@
 
 #endif // defined(__MBOOT2__)
 
-void kmain(unsigned long magic, unsigned long  mboot_info_addr __ATTR_MAYBE_UNUSED)
+extern uint32_t kernel_addr_base;
+extern uint32_t kernel_addr_end;
+
+void kmain(uint32_t magic, uint32_t mbi_base_addr)
 {
     tty_init();
 
-    if(magic != BL_MAGIC)
-    {
-        abandon_ship("Expected boot spec " BOOT_SPEC_NAME " was not matched\n");
-    }
-
-    printf("Boot spec is %s\n", BOOT_SPEC_NAME);
-
-    printf(
+    kprintf(
         "Codename %s version %d.%d.%d\n", 
         __KCODENAME__,
         __KVER_MAJ__,
         __KVER_MIN__,
         __KPATCH_N__
     );
+
+    kprintf(
+        "Kernel base: 0x%X, size: 0x%X\n",
+        &kernel_addr_base,
+        &kernel_addr_end - &kernel_addr_base
+    );
+
+    if(magic != BL_MAGIC)
+    {
+        abandon_ship("Expected boot spec " BOOT_SPEC_NAME " was not matched\n");
+    }
+
+    kprintf("Boot spec is %s\n", BOOT_SPEC_NAME);
+
+    mboot_mbi *mbi = (mboot_mbi*)mbi_base_addr;
+    mboot_mmap *mmap = NULL;
+    kprintf("\nmemory map: \n");
+    for(
+        mmap = (mboot_mmap *)mbi->mmap_base_addr;
+        (U32)mmap < mbi->mmap_base_addr + mbi->mmap_length;
+        mmap = (mboot_mmap *)((U32)mmap + mmap->size + sizeof(mmap->size))
+    )
+    {
+        kprintf("base 0x%X ", mmap->base_addr);
+        kprintf("len 0x%X ", mmap->length);
+        kprintf("type %d ", mmap->type);
+        kprintf("size %d\n", mmap->size);
+    }
 }
