293079e666aa07455febd1993bd59f52427a2f87
oda404 <xdxalexandru404@gmail.com>
2021-04-24 15:33:29 +0300

    [x86] Move unused GDT macros and function decls from the header file into the compilation unit.

--- arch/x86/gdt/gdt.c
+++ arch/x86/gdt/gdt.c
@@ -4,6 +4,8 @@
 */
 
 #include<dxgmx/x86/gdt.h>
+#include<stdint.h>
+#include<dxgmx/gcc/attrs.h>
 
 /* set to 1 by the cpu if 
  * the segment is accessed,
@@ -41,6 +43,41 @@
 /* 1 for long mode */
 #define SEG_LONG(n)      ((n & 1) << 1)
 
+/*
+    lgdt expects a 2 byte limit (size in bytes of the gdt)
+    and it's base address in 4 bytes
+*/
+typedef struct
+__ATTR_PACKED
+{
+    uint16_t limit;
+    uint32_t base;
+} GDTR;
+
+/*
+ * Format of a GDT entry - 64 bits
+ * 
+ * |63            56|55    52|51    48|47            40|39            32|
+ * |----------------+--------+--------+----------------+----------------|
+ * |   base 56-63   | flags  |lim16-19|  access byte   |   base 16-23   |
+ * |----------------+--------+--------+----------------+----------------|
+ * |            base 16-31            |           limit 0-15            |
+ * |----------------------------------+---------------------------------|
+ * |31                              16|15                              0|
+*/
+/* defined here as a packed struct for easier access */
+typedef struct
+__ATTR_PACKED
+{
+    uint16_t limit_0_15;
+    uint16_t base_0_15;
+    uint8_t  base_16_23;
+    uint8_t  access_byte;
+    uint8_t  limit_16_19:4;
+    uint8_t  flags:4;
+    uint8_t  base_24_31;
+} GDTEntry;
+
 #define GDT_ENTRIES_CNT 5
 static GDTEntry gdt[GDT_ENTRIES_CNT];
 
@@ -48,6 +85,24 @@ static GDTEntry gdt[GDT_ENTRIES_CNT];
 extern void load_gdtr(GDTR *gdtr);
 
 //note: bit 0 of flags is currently unused
+
+static void gdt_encode_entry(
+    uint32_t base,
+    uint32_t limit,
+    uint8_t access_byte,
+    uint8_t flags,
+    GDTEntry *target
+)
+{
+    target->limit_0_15  = (limit & 0xFFFF);
+    target->base_0_15   = (base & 0xFFFF);
+
+    target->base_16_23  = (base & 0xFF0000) >> 16;
+    target->access_byte = access_byte;
+    target->limit_16_19 = (limit & 0xF0000) >> 16;
+    target->flags       = (flags & 0xF);
+    target->base_24_31  = (base & 0xFF000000) >> 24;
+}
 
 void gdt_init()
 {
@@ -92,22 +147,4 @@ void gdt_init()
     gdtr.limit = sizeof(gdt);
 
     load_gdtr(&gdtr);
-}
-
-void gdt_encode_entry(
-    uint32_t base,
-    uint32_t limit,
-    uint8_t access_byte,
-    uint8_t flags,
-    GDTEntry *target
-)
-{
-    target->limit_0_15  = (limit & 0xFFFF);
-    target->base_0_15   = (base & 0xFFFF);
-
-    target->base_16_23  = (base & 0xFF0000) >> 16;
-    target->access_byte = access_byte;
-    target->limit_16_19 = (limit & 0xF0000) >> 16;
-    target->flags       = (flags & 0xF);
-    target->base_24_31  = (base & 0xFF000000) >> 24;
 }
