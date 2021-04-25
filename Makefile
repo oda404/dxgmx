6af41208e7c68b73e6c8ca1a08dc7a8f0eca528f
oda404 <xdxalexandru404@gmail.com>
2021-04-25 16:05:51 +0300

    [include] Add kdefs header

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 VER_MAJ           := 0
 VER_MIN           := 5
-PATCH_N           := 4
+PATCH_N           := 5
 CODE_NAME         := angel_attack
 
 DEFAULT_ARCH      := x86
---
+++ include/dxgmx/kdefs.h
@@ -0,0 +1,18 @@
+
+#ifndef __DXGMX_KCONFIG_H__
+#define __DXGMX_KCONFIG_H__
+
+#ifndef __KVER_MAJ__
+#define __KVER_MAJ__ -1
+#endif
+#ifndef __KVER_MIN__
+#define __KVER_MIN__ -1
+#endif
+#ifndef __KPATCH_N__
+#define __KPATCH_N__ -1
+#endif
+#ifndef __KCODENAME__
+#define __KCODENAME__ "undefined"
+#endif
+
+#endif // __DXGMX_KCONFIG_H__
