31d77230cf8a064d4f1543d63d40770fe67950f5
oda404 <xdxalexandru404@gmail.com>
2021-04-28 18:28:02 +0300

    [x86] Add page frame declarations + implementation skeleton

---
+++ arch/x86/paging/frame.c
@@ -0,0 +1,17 @@
+
+#include<dxgmx/pgframe.h>
+
+void pgframe_alloc_init()
+{
+    
+}
+
+int  pgframe_alloc()
+{
+
+}
+
+void pgframe_free()
+{
+
+}
---
+++ include/dxgmx/pgframe.h
@@ -0,0 +1,9 @@
+
+#ifndef __DXGMX_PGFRAME_H__
+#define __DXGMX_PGFRAME_H__
+
+void pgframe_alloc_init();
+int  pgframe_alloc();
+void pgframe_free();
+
+#endif // __DXGMX_PGFRAME_H__
