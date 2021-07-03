75e724e91305c2da5fdcf15bad156ada40e9b791
oda404 <xdxalexandru404@gmail.com>
2021-07-03 14:40:50 +0300

    [include/dxgmx/stdio.h] Add _ATTR_FMT_PRINTF and _ATTR_NONNULL to kprintf.

--- include/dxgmx/stdio.h
+++ include/dxgmx/stdio.h
@@ -6,6 +6,9 @@
 #ifndef __DXGMX_KSTDIO_H__
 #define __DXGMX_KSTDIO_H__
 
-int kprintf(const char *fmt, ...);
+#include<dxgmx/attrs.h>
+
+int kprintf(const char *fmt, ...)
+_ATTR_FMT_PRINTF(1, 2) _ATTR_NONNULL(1);
 
 #endif // __DXGMX_KSTDIO_H__
