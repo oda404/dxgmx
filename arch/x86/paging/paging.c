4d45f68dda778e347c8ce4a37c20ffffd9d0f3a7
oda404 <xdxalexandru404@gmail.com>
2021-07-15 12:20:13 +0300

    Add arch/x86/paging/paging.c include/dxgmx/paging/paging.h. Add paging_enable function.

---
+++ arch/x86/paging/paging.c
@@ -0,0 +1,14 @@
+
+#include<dxgmx/paging/paging.h>
+
+void paging_enable()
+{
+    asm volatile(
+        "movl $1,   %eax  \n"
+        "movl %cr0, %ebx  \n"
+        "or   %eax, %ebx  \n"
+        "shl  $31,  %eax  \n"
+        "or   %eax, %ebx  \n"
+        "movl %ebx, %cr0  \n"
+    );
+}
--- include/dxgmx/paging/paging.h
+++ include/dxgmx/paging/paging.h
@@ -2,10 +2,6 @@
 #ifndef _DXGMX_PAGING_H
 #define _DXGMX_PAGING_H
 
-#include<dxgmx/paging/pagedir.h>
-#include<dxgmx/paging/pagetable.h>
-#include<dxgmx/paging/pagesize.h>
-#include<dxgmx/paging/pageflags.h>
-#include<dxgmx/paging/pageframe.h>
+void paging_enable();
 
 #endif //_DXGMX_PAGING_H
