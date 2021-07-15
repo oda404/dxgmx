a73db097fd196d01002e53f2e1b0e2e33c09d443
oda404 <xdxalexandru404@gmail.com>
2021-07-15 12:13:17 +0300

    [pagetable] Fix PageTableEntry struct. Add functions to interact with the PageTableEntry and PageTable structs.

--- arch/x86/paging/pagetable.c
+++ arch/x86/paging/pagetable.c
@@ -1,8 +1,119 @@
 
 #include<dxgmx/paging/pagetable.h>
+#include<dxgmx/bitwise.h>
+#include<dxgmx/paging/pagesize.h>
 #include<dxgmx/string.h>
 
 void pagetable_init(PageTable *table)
 {
-    memset((void *)table, 0, sizeof(table));
+    memset((void *)table, 0, sizeof(PageTable));
+}
+
+void pagetable_entry_set_present(
+    int present,
+    PageTableEntry *entry
+)
+{
+    entry->present = present;
+}
+
+void pagetable_entry_set_rw(
+    int rw,
+    PageTableEntry *entry
+)
+{
+    entry->rw = rw;
+}
+
+void pagetable_entry_set_user_access(
+    int user_acces,
+    PageTableEntry *entry
+)
+{
+    entry->user_access = user_acces;
+}
+
+void pagetable_entry_set_write_through(
+    int write_through,
+    PageTableEntry *entry
+)
+{
+    entry->write_through = write_through;
+}
+
+void pagetable_entry_set_cache_disabled(
+    int cache_disabled,
+    PageTableEntry *entry
+)
+{
+    entry->cache_disabled = cache_disabled;
+}
+
+void pagetable_entry_set_pat_memtype(
+    int pat_memtype,
+    PageTableEntry *entry
+)
+{
+    entry->pat_memtype = pat_memtype;
+}
+
+void pagetable_entry_set_global(
+    int global,
+    PageTableEntry *entry
+)
+{
+    entry->global = global;
+}
+
+int pagetable_entry_set_frame_base(
+    uint32_t frame_base, 
+    PageTableEntry *entry
+)
+{
+    if(!bw_is_aligned(frame_base, PAGE_SIZE))
+        return 1;
+
+    entry->frame_base = frame_base >> 12;
+
+    return 0;
+}
+
+uint32_t pagetable_entry_get_frame_base(PageTableEntry *entry)
+{
+    return entry->frame_base;
+}
+
+int pagetable_entry_get_present(PageTableEntry *entry)
+{
+    return entry->present;
+}
+
+int pagetable_entry_get_rw(PageTableEntry *entry)
+{
+    return entry->rw;
+}
+
+int pagetable_entry_get_user_access(PageTableEntry *entry)
+{
+    return entry->user_access;
+}
+
+int pagetable_entry_get_write_through(PageTableEntry *entry)
+{
+    return entry->write_through;
+}
+
+int pagetable_entry_get_cache_disabled(PageTableEntry *entry)
+{
+    return entry->cache_disabled;
+}
+
+int pagetable_entry_get_pat_memtype(PageTableEntry *entry)
+{
+    return entry->pat_memtype;
+}
+
+int pagetable_entry_get_global(PageTableEntry *entry)
+{
+    return entry->global;
 }
--- include/dxgmx/paging/pagetable.h
+++ include/dxgmx/paging/pagetable.h
@@ -14,31 +14,33 @@
  * |----------------------------------------+------+--+--+--+--+--+--+--+--+--|
 */
 
+
+
 typedef struct
 _ATTR_PACKED S_PageTableEntry
 {
-    /* This is a 4KiB aligned address to a page frame. */
-    uint32_t frame_base: 20;
-    /* Free for use :) */
-    uint8_t unused: 3;
-    /* Set: page will not be deleted from cache if CR3 changes. */
-    uint8_t global: 1;
-    /**/
-    uint8_t pat_memtype: 1;
-    /* Set if the page has been written to. */
-    uint8_t dirty: 1;
-    /* Set if the page has been read/written to. */
-    uint8_t accessed: 1;
-    /* Set if caching for this page is disabled. */
-    uint8_t cache_disabled: 1;
-    /* Set if write_through is enabled for this page. */
-    uint8_t write_through: 1;
-    /* Set if the page can be accessed by users. */
-    uint8_t user_access: 1;
-    /* Set if the page is read/write. */
-    uint8_t rw: 1;
-    /* Set if the page is present in physical memory. */
-    uint8_t present: 1;
+    /* 1 if the page is present in memory. */
+    uint8_t  present:        1;
+    /* 1 if the page is read/write, 0 if it's read only. */
+    uint8_t  rw:             1;
+    /* 1 if the page can be accessed by both kernel and users, 0 if the page can only be accessed by the kernel. */
+    uint8_t  user_access:    1;
+    /* 1 if the page has write through enabled. */
+    uint8_t  write_through:  1;
+    /* 1 if the page has caching disabled. */
+    uint8_t  cache_disabled: 1;
+    /* Set to 1 by the CPU if the page has been read/written to. Must be manually cleared if needed. */
+    uint8_t  accessed:       1;
+    /* Set to 1 by the CPU if the page has been written to. Must be manually cleared if needed. */
+    uint8_t  dirty:          1;
+    /* If PAT is supported it indicates the memory type. Otherwise must be 0. */
+    uint8_t  pat_memtype:    1;
+    /* 1 if the page should be kept in the TLB between page dir flushes. */
+    uint8_t  global:         1;
+    /* Free for use. */
+    uint8_t  unused:         3;
+    /* 4KiB or 4MiB aligned address of the page frame. */
+    uint32_t frame_base:     20;
 } PageTableEntry;
 
 typedef struct S_PageTable
@@ -47,5 +49,23 @@ typedef struct S_PageTable
 } PageTable;
 
 void pagetable_init(PageTable *table);
+
+void pagetable_entry_set_present(int present, PageTableEntry *entry);
+void pagetable_entry_set_rw(int rw, PageTableEntry *entry);
+void pagetable_entry_set_user_access(int user_acces,PageTableEntry *entry);
+void pagetable_entry_set_write_through(int write_through, PageTableEntry *entry);
+void pagetable_entry_set_cache_disabled(int cache_disabled, PageTableEntry *entry);
+void pagetable_entry_set_pat_memtype(int pat_memtype, PageTableEntry *entry);
+void pagetable_entry_set_global(int global, PageTableEntry *entry);
+int  pagetable_entry_set_frame_base(uint32_t frame_base,  PageTableEntry *entry);
+
+int pagetable_entry_get_present(PageTableEntry *entry);
+int pagetable_entry_get_rw(PageTableEntry *entry);
+int pagetable_entry_get_user_access(PageTableEntry *entry);
+int pagetable_entry_get_write_through(PageTableEntry *entry);
+int pagetable_entry_get_cache_disabled(PageTableEntry *entry);
+int pagetable_entry_get_pat_memtype(PageTableEntry *entry);
+int pagetable_entry_get_global(PageTableEntry *entry);
+uint32_t pagetable_entry_get_frame_base(PageTableEntry *entry);
 
 #endif //_DXGMX_PAGING_PAGETABLE_H
