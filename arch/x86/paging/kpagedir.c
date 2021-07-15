28a10be4d346c9d37a7b7b6f5a641ecf4840c138
oda404 <xdxalexandru404@gmail.com>
2021-07-15 12:23:04 +0300

    Add arch/x86/paging/kpagedir.c and include/dxgmx/paging/kpagedir.h which contains the kernel paging directory and enables paging.

---
+++ arch/x86/paging/kpagedir.c
@@ -0,0 +1,44 @@
+
+#include<dxgmx/paging/kpagedir.h>
+#include<dxgmx/paging/pagedir.h>
+#include<dxgmx/paging/pagetable.h>
+#include<dxgmx/paging/paging.h>
+#include<dxgmx/paging/pagesize.h>
+#include<dxgmx/kprintf.h>
+#include<dxgmx/attrs.h>
+#include<stddef.h>
+#include<stdbool.h>
+
+static PageDirectory pagedir _ATTR_ALIGNED(PAGE_SIZE);
+static PageTable pagetables _ATTR_ALIGNED(PAGE_SIZE);
+
+PageDirectory *kpagedir_get()
+{
+    return &pagedir;
+}
+
+int kpagedir_init()
+{
+    pagedir_init(&pagedir);
+    pagetable_init(&pagetables);
+
+    /* Identity map the first 4MiB */
+    for(size_t i = 0; i < 1024; ++i)
+    {
+        PageTableEntry *entry = &pagetables.entries[i];
+        pagetable_entry_set_frame_base(i * PAGE_SIZE, entry);
+        pagetable_entry_set_present(true, entry);
+        pagetable_entry_set_rw(true, entry);
+    }
+
+    pagedir_entry_set_table_base(&pagetables, &pagedir.entries[0]);
+    pagedir_entry_set_present(true, &pagedir.entries[0]);
+    pagedir_entry_set_rw(true, &pagedir.entries[0]);
+
+    pagedir_load(&pagedir);
+    paging_enable();
+
+    kprintf("Entering paging prison...\n");
+
+    return 0;
+}
---
+++ include/dxgmx/paging/kpagedir.h
@@ -0,0 +1,10 @@
+
+#ifndef _DXGMX_PAGING_KPAGEDIR_H
+#define _DXGMX_PAGING_KPAGEDIR_H
+
+#include<dxgmx/paging/pagedir.h>
+
+PageDirectory *kpagedir_get();
+int kpagedir_init();
+
+#endif //_DXGMX_PAGING_KPAGEDIR_H
