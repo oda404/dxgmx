a8a6dbfd0a587da433a26917eaf0c6153ca0ea23
oda404 <xdxalexandru404@gmail.com>
2021-07-15 14:40:13 +0300

    [kernel/string/strcpy.c] Fix strcpy calling strlen(src) in it's second statement everytime.

--- kernel/string/strcpy.c
+++ kernel/string/strcpy.c
@@ -8,7 +8,8 @@
 char *strcpy(char *dest, const char *src)
 {
     size_t i = 0;
-    for(; i < strlen(src); ++i)
+    size_t len = strlen(src);
+    for(; i < len; ++i)
     {
         dest[i] = src[i];
     }
