95e091cee0f84ccb74401f30fbeca27c9e9287f9
oda404 <xdxalexandru404@gmail.com>
2021-07-23 11:52:05 +0300

    [include/dxgmx/assert.h] Add include/dxgmx/assert.h with ASSERT macro.

---
+++ include/dxgmx/assert.h
@@ -0,0 +1,13 @@
+
+#ifndef _DXGMX_ASSERT_H
+#define _DXGMX_ASSERT_H
+
+#include<dxgmx/abandon_ship.h>
+
+#define ASSERT(x) \
+if(!x)            \
+{                 \
+    abandon_ship("Assertion failed '%s' in %s at %s:%d.\n", #x, __FILE__, __FUNCTION__, __LINE__); \
+}
+
+#endif //_DXGMX_ASSERT_H_
