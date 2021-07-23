6afb6c1f328888bd985074a668c1f5df7d8e6e2a
oda404 <xdxalexandru404@gmail.com>
2021-07-23 15:52:14 +0300

    [include/dxgmx/x86/sysgdt.h] Add macros for the system gdt descriptors..

--- include/dxgmx/x86/sysgdt.h
+++ include/dxgmx/x86/sysgdt.h
@@ -2,6 +2,11 @@
 #ifndef _DXGMX_X86_GDT_SYSGDT_H
 #define _DXGMX_X86_GDT_SYSGDT_H
 
+#define SYSGDT_KERNEL_CS 0x8
+#define SYSGDT_KERNEL_DS 0x10
+#define SYSGDT_USER_CS   0x18
+#define SYSGDT_USER_DS   0x20
+
 void sysgdt_init();
 
 #endif //_DXGMX_X86_GDT_SYSGDT_H
