1f5a5fffbd0b164df5cb5b8b34249970dbbd62d5
oda404 <xdxalexandru404@gmail.com>
2021-08-08 23:47:43 +0300

    [x86-cmos] Add cmos_port_outb function. cmos_port_inb now explicitly needs the NMIs state in 'nmistate'. Add NMIState enum for NMIs states. Tweak implementations.

--- arch/x86/cmos.c
+++ arch/x86/cmos.c
@@ -1,6 +1,7 @@
 
 #include<dxgmx/x86/cmos.h>
 #include<dxgmx/x86/portio.h>
+#include<dxgmx/types.h>
 
 /** 
  * Because of ancient computers the switch for disabling
@@ -8,29 +9,76 @@
  * If this bit is set NMIs will be masked(ignored).
 */
 
+#define NMI_ENABLE(x)  (x & 0x7F)
+#define NMI_DISABLE(x) (x | 0x80)
+
+static u8 g_nmienabled = 1;
+
 void cmos_disable_nmi()
 {
-    port_outb(
-        port_inb(CMOS_PORT_REG) | (1 << 7), 
-        CMOS_PORT_REG
-    );
+    g_nmienabled = 0;
+    port_outb(NMI_DISABLE(port_inb(CMOS_PORT_REG)), CMOS_PORT_REG);
 }
 
 void cmos_enable_nmi()
 {
-    port_outb(
-        port_inb(CMOS_PORT_REG) & ~(1 << 7), 
-        CMOS_PORT_REG
-    );
+    g_nmienabled = 1;
+    port_outb(NMI_ENABLE(port_inb(CMOS_PORT_REG)), CMOS_PORT_REG);
 }
 
 int cmos_is_nmi_enabled()
 {
-    return port_inb(CMOS_PORT_REG) & (1 << 7);
+    return g_nmienabled;
 }
 
-uint8_t cmos_port_inb(uint8_t port)
+uint8_t cmos_port_inb(uint8_t port, int nmistate)
 {
+    switch(nmistate)
+    {
+    case NMIENABLED:
+        g_nmienabled = 1;
+        port = NMI_ENABLE(port);
+        break;
+
+    case NMIDISABLED:
+        g_nmienabled = 0;
+        port = NMI_DISABLE(port);
+        break;
+
+    case NMIKEEP:
+        port = (g_nmienabled ? NMI_ENABLE(port) : NMI_DISABLE(port));
+        break;
+
+    default:
+        break;
+    }
+
     port_outb(port, CMOS_PORT_REG);
     return port_inb(CMOS_PORT_DATA);
+}
+
+void cmos_port_outb(uint8_t value, uint8_t port, int nmistate)
+{
+    switch(nmistate)
+    {
+    case NMIENABLED:
+        g_nmienabled = 1;
+        port = NMI_ENABLE(port);
+        break;
+
+    case NMIDISABLED:
+        g_nmienabled = 0;
+        port = NMI_DISABLE(port);
+        break;
+
+    case NMIKEEP:
+        port = (g_nmienabled ? NMI_ENABLE(port) : NMI_DISABLE(port));
+        break;
+
+    default:
+        break;
+    }
+
+    port_outb(port, CMOS_PORT_REG);
+    port_outb(value, CMOS_PORT_DATA);
 }
--- include/dxgmx/x86/cmos.h
+++ include/dxgmx/x86/cmos.h
@@ -7,9 +7,23 @@
 #define CMOS_PORT_REG      0x70
 #define CMOS_PORT_DATA     0x71
 
+typedef enum
+E_NMIState
+{
+    /* NMIs are enabled. */
+    NMIENABLED  = 0,
+    /* NMIs are disabled */
+    NMIDISABLED = 1,
+    /* NMIs are kept the way they are */
+    NMIKEEP     = 2,
+    /* NMIs state is manually specified in the 'value' argument. */
+    MMIMAN      = 3
+} NMIState;
+
 void cmos_disable_nmi();
 void cmos_enable_nmi();
 int cmos_is_nmi_enabled();
-uint8_t cmos_port_inb(uint8_t port);
+uint8_t cmos_port_inb(uint8_t port, int nmistate);
+void cmos_port_outb(uint8_t value, uint8_t port, int nmistate);
 
 #endif //_DXGMX_X86_CMOS_H
