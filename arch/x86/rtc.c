f46f8f8a95591818a290fe8fb08d6360786e5344
oda404 <xdxalexandru404@gmail.com>
2021-08-09 01:04:22 +0300

    [x86-rtc] Tweak rtc API. Replace the RTCTimeInfo struct with struct tm. Start timekeeping using IRQ8 in a Timespec struct. Replace port_outb calls with cmos_port_outb.

--- arch/x86/rtc.c
+++ arch/x86/rtc.c
@@ -1,4 +1,3 @@
-
 #include<dxgmx/x86/rtc.h>
 #include<dxgmx/x86/cmos.h>
 #include<dxgmx/x86/sysidt.h>
@@ -12,13 +11,13 @@
 #define RTC_REG_B 0xB
 #define RTC_REG_C 0xC
 
-#define RTC_REG_SECONDS  0x0
-#define RTC_REG_MINUTES  0x2
-#define RTC_REG_HOURS    0x4
-#define RTC_REG_WEEKDAY  0x6
-#define RTC_REG_MONTHDAY 0x7
-#define RTC_REG_MONTH    0x8
-#define RTC_REG_YEAR     0x9
+#define RTC_REG_SECONDS  0x0 /* seconds [0-59]        */
+#define RTC_REG_MINUTES  0x2 /* minutes [0-59]        */
+#define RTC_REG_HOURS    0x4 /* hours   [0-23]/[1-12] */
+#define RTC_REG_WEEKDAY  0x6 /* wday    [1-7]         */
+#define RTC_REG_MONTHDAY 0x7 /* mday    [1-31]        */
+#define RTC_REG_MONTH    0x8 /* month   [1-12]        */
+#define RTC_REG_YEAR     0x9 /* year    [0-99]        */
 
 typedef enum
 E_RTCRegisterCFlags
@@ -33,7 +32,7 @@ E_RTCRegisterBFlags
     RTC_REG_B_BINARY_MODE = (1 << 2)
 } RTCRegisterBFlags;
 
-static void bcd_to_binary(u8 *val)
+static void bcd_to_binary(int *val)
 {
     *val = ((*val & 0xF0) >> 1) + ((*val & 0xF0) >> 3) + (*val & 0xF);
 }
@@ -45,9 +44,9 @@ static u16 rtc_calculate_freq()
     return 32768 >> ((a & 0b1111) - 1);
 }
 
-static RTCTimeInfo g_rtc_timeinfo;
+static struct tm   g_rtc_tm;
 static u16         g_rtc_freq;
-static Timespec    g_rtc_timespec;
+static Timespec    g_rtc_ts;
 
 void rtc_int_handler(
     const InterruptFrame _ATTR_MAYBE_UNUSED *frame, 
@@ -61,28 +60,36 @@ void rtc_int_handler(
 
     if(c & RTC_REG_C_UPDATE_DONE)
     {
-        g_rtc_timeinfo.seconds  = cmos_port_inb(RTC_REG_SECONDS,  NMIDISABLED);
-        g_rtc_timeinfo.minutes  = cmos_port_inb(RTC_REG_MINUTES,  NMIDISABLED);
-        g_rtc_timeinfo.hours    = cmos_port_inb(RTC_REG_HOURS,    NMIDISABLED);
-        g_rtc_timeinfo.weekday  = cmos_port_inb(RTC_REG_WEEKDAY,  NMIDISABLED);
-        g_rtc_timeinfo.monthday = cmos_port_inb(RTC_REG_MONTHDAY, NMIDISABLED);
-        g_rtc_timeinfo.month    = cmos_port_inb(RTC_REG_MONTH,    NMIDISABLED);
-        g_rtc_timeinfo.year     = cmos_port_inb(RTC_REG_YEAR,     NMIDISABLED);
+        g_rtc_tm.tm_sec  = cmos_port_inb(RTC_REG_SECONDS,  NMIDISABLED);
+        g_rtc_tm.tm_min  = cmos_port_inb(RTC_REG_MINUTES,  NMIDISABLED);
+        g_rtc_tm.tm_hour = cmos_port_inb(RTC_REG_HOURS,    NMIDISABLED);
+        g_rtc_tm.tm_wday = cmos_port_inb(RTC_REG_WEEKDAY,  NMIDISABLED) - 1;
+        g_rtc_tm.tm_mday = cmos_port_inb(RTC_REG_MONTHDAY, NMIDISABLED);
+        g_rtc_tm.tm_mon  = cmos_port_inb(RTC_REG_MONTH,    NMIDISABLED) - 1;
+        g_rtc_tm.tm_year = cmos_port_inb(RTC_REG_YEAR,     NMIDISABLED);
+        g_rtc_tm.tm_yday = 0; //FIXME
+        g_rtc_tm.tm_isdst = 0; //FIXME
 
         if(!(b & RTC_REG_B_BINARY_MODE))
         {
-            bcd_to_binary(&g_rtc_timeinfo.seconds);
-            bcd_to_binary(&g_rtc_timeinfo.minutes);
-            bcd_to_binary(&g_rtc_timeinfo.hours);
-            bcd_to_binary(&g_rtc_timeinfo.weekday);
-            bcd_to_binary(&g_rtc_timeinfo.monthday);
-            bcd_to_binary(&g_rtc_timeinfo.month);
-            bcd_to_binary(&g_rtc_timeinfo.year);
+            bcd_to_binary(&g_rtc_tm.tm_sec);
+            bcd_to_binary(&g_rtc_tm.tm_min);
+            bcd_to_binary(&g_rtc_tm.tm_hour);
+            bcd_to_binary(&g_rtc_tm.tm_wday);
+            bcd_to_binary(&g_rtc_tm.tm_mday);
+            bcd_to_binary(&g_rtc_tm.tm_mon);
+            bcd_to_binary(&g_rtc_tm.tm_year);
         }
+
+        /** 
+         * Make g_rtc_tm.tm_year be the years since 1900 as per spec. 
+         * Just assume we are in the 2000's. 2000 - 1900 == 100.
+        */
+        g_rtc_tm.tm_year += 100;
 
         if(!(b & RTC_REG_B_24HOUR_FMT))
         {
-            if(g_rtc_timeinfo.hours & (1 << 7))
+            if(g_rtc_tm.tm_hour & (1 << 7))
             {
                 //PM
                 TODO();
@@ -93,7 +100,7 @@ void rtc_int_handler(
     }
     else
     {
-        timespec_add(1.f / g_rtc_freq * 1000000000, &g_rtc_timespec);
+        timespec_add(1.f / g_rtc_freq * 1000000000, &g_rtc_ts);
     }
 
     asm volatile("sti");
@@ -101,15 +108,15 @@ void rtc_int_handler(
 
 int rtc_init()
 {
-    memset(&g_rtc_timeinfo, 0, sizeof(RTCTimeInfo));
-    memset(&g_rtc_timespec, 0, sizeof(Timespec));
+    memset(&g_rtc_tm, 0, sizeof(struct tm));
+    memset(&g_rtc_ts, 0, sizeof(Timespec));
     g_rtc_freq = rtc_calculate_freq();
 
     sysidt_register_callback(IRQ8, rtc_int_handler);
     rtc_enable_irq8();
     // read the C register so we make sure future IRQs will fire.
     cmos_port_inb(RTC_REG_C, NMIENABLED);
-    while(!g_rtc_timeinfo.year /*2100?*/);
+    while(!g_rtc_tm.tm_year /*2100?*/);
 
     return 0;
 }
@@ -125,21 +132,26 @@ void rtc_enable_irq8()
     asm volatile("sti");
 }
 
-void rtc_dump_timeinfo()
+void rtc_dump_time_and_date()
 {
     klog(
         KLOG_INFO,
-        "[RTC] Current date: %02d:%02d:%02d %02d/%02d/20%d\n",
-        g_rtc_timeinfo.hours,
-        g_rtc_timeinfo.minutes,
-        g_rtc_timeinfo.seconds,
-        g_rtc_timeinfo.monthday,
-        g_rtc_timeinfo.month,
-        g_rtc_timeinfo.year
+        "[RTC] Current time and date: %02d:%02d:%02d %02d/%02d/%d.\n",
+        g_rtc_tm.tm_hour,
+        g_rtc_tm.tm_min,
+        g_rtc_tm.tm_sec,
+        g_rtc_tm.tm_mday,
+        g_rtc_tm.tm_mon + 1,
+        1900 + g_rtc_tm.tm_year
     );
 }
 
-const RTCTimeInfo *rtc_get_timeinfo()
+const Timespec *rtc_get_running_ts()
 {
-    return &g_rtc_timeinfo;
+    return &g_rtc_ts;
+}
+
+const struct tm *rtc_get_tm()
+{
+    return &g_rtc_tm;
 }
--- include/dxgmx/x86/rtc.h
+++ include/dxgmx/x86/rtc.h
@@ -3,22 +3,16 @@
 #define _DXGMX_X86_RTC_H
 
 #include<stdint.h>
-
-typedef struct
-S_RTCTimeInfo
-{
-    uint8_t seconds;
-    uint8_t minutes;
-    uint8_t hours;
-    uint8_t weekday;
-    uint8_t monthday;
-    uint8_t month;
-    uint8_t year;
-} RTCTimeInfo;
+#include<dxgmx/time.h>
 
 int rtc_init();
 void rtc_enable_irq8();
-void rtc_dump_timeinfo();
-const RTCTimeInfo *rtc_get_timeinfo();
+void rtc_dump_time_and_date();
+/**
+ * Returns a ptr to the running Timespec struct which is 
+ * updated every 1 / rtc_freq seconds.
+*/
+const Timespec *rtc_get_running_ts();
+const struct tm *rtc_get_tm();
 
 #endif //_DXGMX_X86_RTC_H
