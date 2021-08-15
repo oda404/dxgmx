d03fd13453143a60619eb66dfd24c7081cef4b00
oda404 <xdxalexandru404@gmail.com>
2021-08-15 15:49:05 +0300

    [x86-int] Add interrupts.h and interrupts.c files with interrupts_enable and interrupts_disable functions.

---
+++ arch/x86/int/interrupts.c
@@ -0,0 +1,18 @@
+
+#include<dxgmx/x86/interrupts.h>
+#include<dxgmx/x86/pic.h>
+#include<dxgmx/attrs.h>
+
+_ATTR_ALWAYS_INLINE void 
+interrupts_disable()
+{
+    pic8259_set_mask(0xFF, 0);
+    pic8259_set_mask(0xFF, 1);
+}
+
+_ATTR_ALWAYS_INLINE void 
+interrupts_enable()
+{
+    pic8259_set_mask(0, 0);
+    pic8259_set_mask(0, 1);
+}
---
+++ include/dxgmx/x86/interrupts.h
@@ -0,0 +1,8 @@
+
+#ifndef _DXGMX_X86_INTERRUPTS_H
+#define _DXGMX_X86_INTERRUPTS_H
+
+void interrupts_disable();
+void interrupts_enable();
+
+#endif //!_DXGMX_X86_INTERRUPTS_H
