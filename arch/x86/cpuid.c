402fa749cd47aa5519f28b86c8977b6155f755b7
oda404 <xdxalexandru404@gmail.com>
2021-08-17 12:25:40 +0300

    [x86-cpuid] Remove cpuid_get_vendor_str and cpuid_get_eax_max functions.

--- arch/x86/cpuid.c
+++ arch/x86/cpuid.c
@@ -28,24 +28,3 @@ int cpuid_is_avail()
     );
     return ret;
 }
-
-void cpuid_get_vendor_str(char *str)
-{
-    uint32_t dead;
-    uint32_t *b = (uint32_t *)&str[0];
-    uint32_t *c = (uint32_t *)&str[4];
-    uint32_t *d = (uint32_t *)&str[8];
-
-    CPUID(CPUID_VENDOR_STR, dead, *b, *d, *c);
-    str[12] = '\0';
-}
-
-uint32_t cpuid_get_eax_max()
-{
-    uint32_t dead;
-    uint32_t ret;
-
-    CPUID(CPUID_MAX_EAX, ret, dead, dead, dead);
-
-    return ret;
-}
--- include/dxgmx/x86/cpuid.h
+++ include/dxgmx/x86/cpuid.h
@@ -6,12 +6,8 @@
 #ifndef _DXGMX_X86_CPUID_H
 #define _DXGMX_X86_CPUID_H
 
-#include<stdint.h>
-
 #define CPUID(func, eax, ebx, ecx, edx) asm volatile("cpuid": "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx): "a"(func));
 
 int cpuid_is_avail();
-void cpuid_get_vendor_str(char *str);
-uint32_t cpuid_get_eax_max();
 
 #endif //_DXGMX_X86_CPUID_H
