63492b48de8e30031a26834d29391bc9e47b630e
oda404 <xdxalexandru404@gmail.com>
2021-08-24 17:03:34 +0300

    [arch/x86/mem/kpaging.c] Add exception handler for page faults. Remove some usless includes.

--- arch/x86/mem/kpaging.c
+++ arch/x86/mem/kpaging.c
@@ -3,6 +3,9 @@
  * Distributed under the MIT license.
 */
 
+#include<dxgmx/x86/sysidt.h>
+#include<dxgmx/cpu.h>
+#include<dxgmx/abandon_ship.h>
 #include<dxgmx/mem/kpaging.h>
 #include<dxgmx/mem/pagedir.h>
 #include<dxgmx/mem/pagetable.h>
@@ -10,11 +13,21 @@
 #include<dxgmx/mem/pagesize.h>
 #include<dxgmx/klog.h>
 #include<dxgmx/attrs.h>
-#include<stddef.h>
-#include<stdbool.h>
 
 static PageDirectory pagedir _ATTR_ALIGNED(PAGE_SIZE);
 static PageTable pagetables _ATTR_ALIGNED(PAGE_SIZE);
+
+static void paging_int_handler(
+    const InterruptFrame _ATTR_MAYBE_UNUSED *frame, 
+    const void _ATTR_MAYBE_UNUSED *data
+)
+{
+    ptr faultaddr = cpu_get_cr2();
+    if(faultaddr == 0)
+        abandon_ship("NULL dereference in ring 0 :(\n");
+    else if(faultaddr < PAGE_SIZE)
+        abandon_ship("Tried to access 0-%d page.\n", PAGE_SIZE);
+}
 
 PageDirectory *kpagedir_get()
 {
@@ -28,23 +41,26 @@ int kpaging_init()
     
     PageTableEntry *entry = &pagetables.entries[0];
     pagetable_entry_set_frame_base(0, entry);
-    pagetable_entry_set_present(false, entry);
-    pagetable_entry_set_rw(false, entry);
+    pagetable_entry_set_present(0, entry);
+    pagetable_entry_set_rw(0, entry);
     
     /* Identity map the first 4MiB */
     for(size_t i = 1; i < 1024; ++i)
     {
         entry = &pagetables.entries[i];
         pagetable_entry_set_frame_base(i * PAGE_SIZE, entry);
-        pagetable_entry_set_present(true, entry);
-        pagetable_entry_set_rw(true, entry);
+        pagetable_entry_set_present(1, entry);
+        pagetable_entry_set_rw(1, entry);
     }
 
     pagedir_entry_set_table_base(&pagetables, &pagedir.entries[0]);
-    pagedir_entry_set_present(true, &pagedir.entries[0]);
-    pagedir_entry_set_rw(true, &pagedir.entries[0]);
+    pagedir_entry_set_present(1, &pagedir.entries[0]);
+    pagedir_entry_set_rw(1, &pagedir.entries[0]);
 
     pagedir_load(&pagedir);
+
+    sysidt_register_callback(ISR14, paging_int_handler);
+
     paging_enable();
 
     klog(KLOG_INFO, "Entering paging prison...\n");
