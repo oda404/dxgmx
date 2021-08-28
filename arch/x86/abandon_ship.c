f5b7e74690f97adcc8307ac03745f8abe4229a75
oda404 <xdxalexandru404@gmail.com>
2021-08-28 14:53:45 +0300

    [abandon_ship] Move abandon_ship.c from kernel/ to arch/x86/. Disable NMIs and interrupts first thing in the abandon_ship call.

--- arch/x86/abandon_ship.c
+++ arch/x86/abandon_ship.c
@@ -3,6 +3,8 @@
  * Distributed under the MIT license.
 */
 
+#include<dxgmx/x86/cmos.h>
+#include<dxgmx/x86/interrupts.h>
 #include<dxgmx/abandon_ship.h>
 #include<dxgmx/kabort.h>
 #include<dxgmx/klog.h>
@@ -10,15 +12,20 @@
 
 void abandon_ship(const char *lastmsg, ...)
 {
-    klog(KLOG_FATAL, "Abandoning ship");
+    cmos_disable_nmi();
+    interrupts_disable();
+
+    klog(KLOG_FATAL, "---[ start of abandon ship ]---\n");
     if(lastmsg)
     {
-        klog(KLOG_FATAL, ": ");
         va_list list;
         va_start(list, lastmsg);
         kvlog(KLOG_FATAL, lastmsg, list);
         va_end(list);
     }
+    else
+        klog(KLOG_FATAL, "nothing to say...\n");
+    klog(KLOG_FATAL, "---[ end of abandon ship ]---\n");
     /* shit and cum my final message goodbye */
     kabort();
 }
