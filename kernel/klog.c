90662948af0d72a9e096ee3c5d011ef50e78a387
oda404 <xdxalexandru404@gmail.com>
2021-08-29 23:42:45 +0300

    [kernel/klog.c] Add a check to see if 'lvl' is 0 when calling klog. Safely set the log level when calling klog_init. Rename g_timer to g_klogtimer.

--- kernel/klog.c
+++ kernel/klog.c
@@ -9,40 +9,37 @@
 #include<dxgmx/timer.h>
 
 static KLogConfig g_klogconfig;
-static Timer      g_timer;
+static Timer      g_klogtimer;
 
 int klog_init(const KLogConfig *kconfig)
 {
     if(!kconfig)
-    {
-        g_klogconfig.loglevel = KLOG_INFO;
-        return 1;
-    }
+        klog_set_max_level(KLOG_INFO); // default to KLOG_INFO if kconfig is NULL.
+    else
+        klog_set_max_level(kconfig->loglevel);
 
-    g_klogconfig = *kconfig;
-
-    timer_init(&g_timer);
-    timer_start(&g_timer);
+    timer_init(&g_klogtimer);
+    timer_start(&g_klogtimer);
 
     return 0;
 }
 
 int klog_set_max_level(uint8_t lvl)
 {
-    if(lvl > KLOG_QUIET)
-        lvl = KLOG_QUIET;
+    if(lvl >= KLOG_ENUM_END)
+        lvl = KLOG_ENUM_END - 1;
     g_klogconfig.loglevel = lvl;
     return lvl;
 }
 
 size_t kvlog(uint8_t lvl, const char *fmt, va_list valist)
 {
-    if(lvl > g_klogconfig.loglevel)
+    if(lvl > g_klogconfig.loglevel || lvl == 0)
         return 0;
 
     size_t written = 0;
 
-    double sec = timer_get_ellapsed_sec(&g_timer);
+    double sec = timer_get_ellapsed_sec(&g_klogtimer);
 
     switch(lvl)
     {
