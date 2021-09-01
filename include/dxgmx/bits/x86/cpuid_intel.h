8e1fdc50361f3991507352007eab9a28b975a8f9
oda404 <xdxalexandru404@gmail.com>
2021-09-01 13:20:53 +0300

    [include/dxgmx/bits/x86/] Add cpuid_intel.h.

---
+++ include/dxgmx/bits/x86/cpuid_intel.h
@@ -0,0 +1,20 @@
+
+#ifndef _DXGMX_BITS_X86_CPUID_INTEL_H
+#define _DXGMX_BITS_X86_CPUID_INTEL_H
+
+#include<dxgmx/types.h>
+
+typedef struct
+S_IntelCPUIDFunc1EDX
+{
+    u8 stepping_id:    4;
+    u8 model_number:   4;
+    u8 family_code:    4;
+    u8 type:           2;
+    u8 reserved:       2;
+    u8 extended_model: 4;
+    u8 extended_family;
+    u8 reserved1:      4;
+} IntelCPUIDFunc1EDX;
+
+#endif //!_DXGMX_BITS_X86_CPUID_INTEL_H
