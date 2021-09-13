1a9b540e822d040b63e76cdaaf5e493178794907
oda404 <xdxalexandru404@gmail.com>
2021-09-13 14:52:55 +0300

    [kernel/stdlib/strtol.c] Actually use the base parameter when converting the string into a long, except for the value 0.

--- kernel/stdlib/strtol.c
+++ kernel/stdlib/strtol.c
@@ -7,10 +7,32 @@
 #include<dxgmx/ctype.h>
 #include<dxgmx/errno.h>
 #include<limits.h>
+#include<dxgmx/todo.h>
+
+static int isbasedigit(int c, int base)
+{    
+    switch(base)
+    {
+    case 2 ... 10:
+        return (c >= '0' && c <= '0' + base - 1);
+    case 11 ... 32:
+        return 
+            (c >= '0' && c <= '9') || 
+            (c >= 'A' && c <= 'A' + base - 11) ||
+            (c >= 'a' && c <= 'a' + base - 11);
+    }
+
+    return 0;
+}
 
 long int strtol(const char *str, char **endptr, int base)
 {
-    // only base 10 for now
+    if((base < 2 && base != 0) || base > 36)
+        return 0;
+
+    if(base == 0)
+        TODO_FATAL(); // can't be fucked now
+    
     int neg = 0;
     /* assume the initial str is invalid */
     if(endptr)
@@ -24,7 +46,7 @@ long int strtol(const char *str, char **endptr, int base)
         ++str;
     }
     /* if a digit isn't found after the potential sign return 0 */
-    if(!isdigit(*str))
+    if(!isbasedigit(*str, base))
     {
         errno = EINVAL;
         return 0;
@@ -34,10 +56,17 @@ long int strtol(const char *str, char **endptr, int base)
 
     for(; *str != '\0'; ++str)
     {
-        if(!isdigit(*str))
+        if(!isbasedigit(*str, base))
             break;
 
-        long int tmp = out * 10 + (*str - '0');
+        long int tmp;
+        if(*str <= '9')
+            tmp = out * base + (*str - '0');
+        else if (*str >= 'a' && *str <= 'v')
+            tmp = out * base + (*str - 'a' + 10);
+        else
+            tmp = out * base + (*str - 'A' + 10);
+
         if(tmp < out) // over or under flow based on sign
         {
             errno = ERANGE;
