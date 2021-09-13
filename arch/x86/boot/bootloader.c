60c98b852b35d871883050a76b61e45367031733
oda404 <xdxalexandru404@gmail.com>
2021-09-13 21:36:53 +0300

    [multiboot] Move mutliboot header from arch/x86/boot/entry.S to it's own file in arch/x86/boot/bootloader.c. Rename the 'multiboot' linker section to 'bootloader'.

---
+++ arch/x86/boot/bootloader.c
@@ -0,0 +1,35 @@
+
+#include<dxgmx/attrs.h>
+
+#if defined(_DXGMX_BOOTLOADER_GRUB_)
+
+#include<dxgmx/x86/multiboot.h>
+
+#define FLAGS (MULTIBOOT_FLAG_MOD_PAGE_ALIGN | MULTIBOOT_FLAG_MEM_INFO | MULTIBOOT_FLAG_VIDEO)
+#define CHECKSUM -(MULTIBOOT_HEADER_MAGIC + FLAGS)
+#define HEADER_ADDR     0
+#define LOAD_ADDR       0
+#define LOAD_END_ADDR   0
+#define BSS_END_ADDR    0
+#define ENTRY_ADDR      0
+#define VIDEO_MODE_TYPE 1
+#define VIDEO_WIDTH     0
+#define VIDEO_HEIGHT    0
+#define VIDEO_DEPTH     0
+
+_ATTR_SECTION(".bootloader") long mbootconfig[] = {
+    MULTIBOOT_HEADER_MAGIC,
+    FLAGS,
+    CHECKSUM,
+    HEADER_ADDR,
+    LOAD_ADDR,
+    LOAD_END_ADDR,
+    BSS_END_ADDR,
+    ENTRY_ADDR,
+    VIDEO_MODE_TYPE,
+    VIDEO_WIDTH,
+    VIDEO_HEIGHT,
+    VIDEO_DEPTH
+};
+
+#endif //defined(_DXGMX_BOOTLOADER_GRUB_)
