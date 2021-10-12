3a38d80d62ead58f488689cf1ad202f6147c856d
oda404 <xdxalexandru404@gmail.com>
2021-10-13 00:21:47 +0300

    [kernel/utils] Add utils/ directory in kernel/ and include/dxgmx/. Add bytes.(c/h) in their corresponding utils/ directory.

---
+++ include/dxgmx/utils/bytes.h
@@ -0,0 +1,12 @@
+
+#ifndef _DXGMX_UITLS_BYTE_H
+#define _DXGMX_UITLS_BYTE_H
+
+#include<dxgmx/types.h>
+
+/* Takes in a number of bytes and transforms them into
+the most human readable format, putting the unit used in 'unit'
+for example: "B", "KiB", "MiB"*/
+float bytes_to_human_readable(u32 bytes, char unit[4]);
+
+#endif //!_DXGMX_UITLS_BYTE_H
---
+++ kernel/utils/bytes.c
@@ -0,0 +1,34 @@
+
+#include<dxgmx/utils/bytes.h>
+#include<dxgmx/string.h>
+
+float bytes_to_human_readable(u32 bytes, char unit[4])
+{
+    float fbytes = bytes;
+    u8 i = 0;
+    while(fbytes > 1024)
+    {
+        ++i;
+        fbytes /= 1024.f; 
+    }
+
+    if(unit)
+    {
+        switch(i)
+        {
+        case 0:
+            strcpy(unit, "B");
+            break;
+        case 1:
+            strcpy(unit, "KiB");
+            break;
+        case 2:
+            strcpy(unit, "MiB");
+            break;
+        case 3:
+            strcpy(unit, "GiB");
+            break;
+        }
+    }
+    return fbytes;
+}
