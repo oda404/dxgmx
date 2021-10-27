4d8a364ba80f2504e4b97887a865a9ca927fbaa2
oda404 <xdxalexandru404@gmail.com>
2021-10-27 23:16:10 +0300

    [x86-timer] Add timer_is_ready() function. Tweak the timer implementation to be safer overall.

--- arch/x86/timer.c
+++ arch/x86/timer.c
@@ -2,78 +2,107 @@
 #include<dxgmx/timer.h>
 #include<dxgmx/x86/rtc.h>
 #include<dxgmx/todo.h>
+#include<dxgmx/string.h>
 #include<dxgmx/stdlib.h>
 
-u8 g_timersrc = 255;
+static u8 g_timersrc;
+static u8 g_timesrc_ready = false;
 
-int timer_find_src()
+_INIT int timer_find_src()
 {
-    g_timersrc = TIMERSOURCE_RTC;
-    return g_timersrc;
-}
-
-void timer_start(Timer *t)
-{
-    for(size_t i = 0; i < 2; ++i)
+    /* the RTC can only be used as a timer if periodic
+    interrupts are enabled. */
+    if(rtc_periodic_ints_enabled())
     {
-        t->src = g_timersrc;
-        switch(t->src)
-        {
-        case TIMERSOURCE_RTC:
-            t->now = rtc_now;
-            break;
-
-        default:
-            timer_find_src();
-            continue;
-        }
-        t->start_ts = t->now();
-        return;
+        g_timersrc = TIMERSOURCE_RTC;
+        g_timesrc_ready = true;
     }
-    abandon_ship("timer: Failed to find a source. Not proceeding.\n");
+    return 0;
 }
 
-struct timespec timer_ellapsed(const Timer *t)
+bool timer_is_ready(const Timer *t)
 {
-    if(g_timersrc != t->src)
+    return t->_is_ready;
+}
+
+int timer_start(Timer *t)
+{
+    memset(t, 0, sizeof(t));
+    t->_is_ready = false;
+
+    if(UNLIKELY(!g_timesrc_ready) && timer_find_src() != 0)
+        return TIMER_START_NO_TIMESOURCE;
+
+    t->_src = g_timersrc;
+    switch(t->_src)
+    {
+    case TIMERSOURCE_RTC:
+        t->_now = rtc_now;
+        break;
+
+    default:
+        return TIMER_START_INVALID_TIMESOURCE;
+    }
+
+    t->_start_ts = t->_now();
+    t->_is_ready = true;
+
+    return TIMER_START_OK;
+}
+
+int timer_ellapsed(struct timespec *ts, const Timer *t)
+{
+    /* If the global timesource is different than the timer's timesource,
+    ??? */
+    if(UNLIKELY(g_timersrc != t->_src))
         TODO_FATAL();
 
-    struct timespec ret;
-    const struct timespec now = t->now();
+    if(UNLIKELY( !timer_is_ready(t) ))
+        return TIMER_ELLAPSED_NEEDS_STARTING;
 
-    ret.tv_sec = now.tv_sec - t->start_ts.tv_sec;
+    const struct timespec now = t->_now();
 
-    time_t nsecdiff = now.tv_nsec - t->start_ts.tv_nsec;
+    ts->tv_sec = now.tv_sec - t->_start_ts.tv_sec;
+
+    time_t nsecdiff = now.tv_nsec - t->_start_ts.tv_nsec;
     if(nsecdiff < 0)
     {
-        --ret.tv_sec;
+        --ts->tv_sec;
         nsecdiff = 1000000000 + nsecdiff;
     }
-    ret.tv_nsec = nsecdiff;
+    ts->tv_nsec = nsecdiff;
 
-    return ret;
+    return TIMER_ELLAPSED_OK;
 }
 
 double timer_ellapsed_sec(const Timer *t)
 {
-    const struct timespec ts = timer_ellapsed(t);
-    return ts.tv_sec + ts.tv_nsec / 1000000000.0;
+    struct timespec ts; 
+    return timer_ellapsed(&ts, t) == TIMER_ELLAPSED_OK ?
+        ts.tv_sec + ts.tv_nsec / 1000000000.0 :
+        -1;
 }
 
 double timer_ellapsed_ms(const Timer *t)
 {
-    const struct timespec ts = timer_ellapsed(t);
-    return ts.tv_sec * 1000 + ts.tv_nsec / 1000000.0;
+    struct timespec ts;
+    return timer_ellapsed(&ts, t) == TIMER_ELLAPSED_OK ?
+        ts.tv_sec * 1000 + ts.tv_nsec / 1000000.0 :
+        -1;
 }
 
 double timer_ellapsed_us(const Timer *t)
 {
-    const struct timespec ts = timer_ellapsed(t);
-    return ts.tv_sec * 1000000 + ts.tv_nsec / 1000.0;
+    struct timespec ts;
+    return timer_ellapsed(&ts, t) == TIMER_ELLAPSED_OK ?
+        ts.tv_sec * 1000000 + ts.tv_nsec / 1000.0 :
+        -1;
 }
 
 double timer_ellapsed_ns(const Timer *t)
 {
-    const struct timespec ts = timer_ellapsed(t);
-    return ts.tv_sec * 1000000000 + ts.tv_nsec;
+    struct timespec ts;
+    return timer_ellapsed(&ts, t) == TIMER_ELLAPSED_OK ?
+        ts.tv_sec * 1000000000 + ts.tv_nsec :
+        -1;
 }
--- include/dxgmx/timer.h
+++ include/dxgmx/timer.h
@@ -14,20 +14,60 @@ typedef struct
 S_Timer
 {
     /* Internal architecture specific timer source. */
-    TimerSources src;
+    TimerSources _src;
     /* Starting timespec struct. */
-    struct timespec start_ts;
+    struct timespec _start_ts;
     /* Internal getter for timespec structs. */
-    timersource now;
+    timersource _now;
+    bool _is_ready;
 } Timer;
 
-int timer_find_src();
-void timer_start(Timer *t);
-struct timespec timer_ellapsed(const Timer *t);
-double timer_ellapsed_sec(const Timer *t);
-double timer_ellapsed_ms(const Timer *t);
-double timer_ellapsed_us(const Timer *t);
-double timer_ellapsed_ns(const Timer *t);
+#define TIMER_START_OK 0
+#define TIMER_START_NO_TIMESOURCE 1
+#define TIMER_START_INVALID_TIMESOURCE 2
 
+#define TIMER_ELLAPSED_OK 0
+#define TIMER_ELLAPSED_NEEDS_STARTING 1
+
+/**
+ * Tries to find the most suitable source to be used as a timer.
+ * @return != 0 if an error occured and no source was found.
+*/
+int timer_find_src();
+/** Starts the timer.
+ * @return See TIMER_START_*.
+*/
+int timer_start(Timer *t);
+/**
+ * @return true if the timer is valid, and can be used. This status is
+ * guaranteed until timer_start() is called again.
+*/
+bool timer_is_ready(const Timer *t);
+/** Puts the ellapsed time since the last timer_start() call 
+ in 'ts'. If the returned value != TIMER_ELLAPSED_OK, the value 
+ of 'ts' is not touched.
+ @return See TIMER_ELLAPSED_*. 
+*/
+int timer_ellapsed(struct timespec *ts, const Timer *t);
+/**
+ * @return < 0 if 't' is not ready. Else the number of seconds since
+ * timer_start() was last called.
+*/
+double timer_ellapsed_sec(const Timer *t);
+/**
+ * @return < 0 if 't' is not ready. Else the number of milliseconds since
+ * timer_start() was last called.
+*/
+double timer_ellapsed_ms(const Timer *t);
+/**
+ * @return < 0 if 't' is not ready. Else the number of microseconds since
+ * timer_start() was last called.
+*/
+double timer_ellapsed_us(const Timer *t);
+/**
+ * @return < 0 if 't' is not ready. Else the number of nanoseconds since
+ * timer_start() was last called.
+*/
+double timer_ellapsed_ns(const Timer *t);
 
 #endif //!_DXGMX_TIMER_H_
