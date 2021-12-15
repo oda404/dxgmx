8bcf5b8a18dece227d72f2b55d51ae536b5bc287
oda404 <xdxalexandru404@gmail.com>
2021-12-15 13:08:18 +0200

    [include/dxgmx/types.h] Remove the (float/double/longdouble)whole typedefs.

--- include/dxgmx/types.h
+++ include/dxgmx/types.h
@@ -10,38 +10,6 @@
 #include<stdint.h>
 #include<stdbool.h>
 
-#if __SIZEOF_FLOAT__ == __SIZEOF_INT__
-typedef int floatwhole;
-#elif __SIZEOF_FLOAT__ == __SIZEOF_LONG__
-typedef long floatwhole;
-#else
-typedef long long floatwhole;
-#endif // __SIZEOF_FLOAT__ == __SIZEOF_INT__
-
-#if __SIZEOF_DOUBLE__ == __SIZEOF_INT__
-typedef int doublewhole;
-#elif __SIZEOF_DOUBLE__ == __SIZEOF_LONG__
-typedef long doublewhole;
-#else
-typedef long long doublewhole;
-#endif // __SIZEOF_DOUBLE__ == __SIZEOF_INT__
-
-#if __SIZEOF_LONG_DOUBLE__ == __SIZEOF_INT__
-typedef int longdoublewhole;
-#elif __SIZEOF_LONG_DOUBLE__ == __SIZEOF_LONG__
-typedef long longdoublewhole;
-#else
-typedef long long longdoublewhole;
-#endif //__SIZEOF_LONG_DOUBLE__ == __SIZEOF_INT__
-
-#if defined(_X86_)
-    typedef uint32_t ptr;
-#   define PTR_DIG 8
-#elif defined(_X86_64_)
-    typedef uint64_t ptr;
-#   define PTR_DIG 16
-#endif //_X86_
-
 typedef uint8_t  u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -50,6 +18,14 @@ typedef int8_t   i8;
 typedef int16_t  i16;
 typedef int32_t  i32;
 typedef int64_t  i64;
+
+#if defined(_X86_)
+    typedef u32 ptr;
+#   define PTR_DIG 8
+#elif defined(_X86_64_)
+    typedef u64 ptr;
+#   define PTR_DIG 16
+#endif //_X86_
 
 #define KIB 1024
 #define MIB (KIB * 1024)
