6e52ff9a16788143854565cde94c14c96a8ffad4
oda404 <xdxalexandru404@gmail.com>
2022-01-09 16:26:51 +0200

    [bitwise] Add bw_is_power_of_two() function.

--- include/dxgmx/utils/bitwise.h
+++ include/dxgmx/utils/bitwise.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Alexandru Olaru.
+ * Copyright 2022 Alexandru Olaru.
  * Distributed under the MIT license.
 */
 
@@ -20,6 +20,7 @@ void bw_set(u64 *n, u8 bit);
 /* returns > 0 if the given number uses more than 32 bits */
 int  bw_is64_wide(u64 n);
 u32 bw_u32_rotl(u32 n, u8 rot);
-u32 bw_u32_flip_endianness(u32 n); 
+u32 bw_u32_flip_endianness(u32 n);
+bool bw_is_power_of_two(u64 n);
 
 #endif // _DXGMX_BITWISE_H
--- kernel/utils/bitwise.c
+++ kernel/utils/bitwise.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Alexandru Olaru.
+ * Copyright 2022 Alexandru Olaru.
  * Distributed under the MIT license.
 */
 
@@ -62,4 +62,14 @@ _ATTR_ALWAYS_INLINE u32
 bw_u32_flip_endianness(u32 n)
 {
     return ((n >> 24) & 0XFF) | ((n << 8) & 0xFF0000) | ((n >> 8) & 0xFF00) | ((n << 24) & 0xFF000000);
+}
+
+_ATTR_ALWAYS_INLINE bool 
+bw_is_power_of_two(u64 n)
+{
+    u8 setbits = 0;
+    for(size_t i = 0; i < 64; ++i)
+        setbits += (n >> i) & 1;
+
+    return setbits == 1;
 }
