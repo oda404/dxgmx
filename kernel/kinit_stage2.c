cc7da444d863c1d58700083a88d1f4d89df85927
oda404 <xdxalexandru404@gmail.com>
2022-02-27 17:37:12 +0200

    [kinit_stage2] Call vfs_init() & panic if it fails.

--- kernel/kinit_stage2.c
+++ kernel/kinit_stage2.c
@@ -5,10 +5,15 @@
 
 #include<dxgmx/types.h>
 #include<dxgmx/attrs.h>
+#include<dxgmx/panic.h>
 #include<dxgmx/cpu.h>
+#include<dxgmx/vfs.h>
 
 _INIT bool kinit_stage2()
 {
+    if(!vfs_init())
+        panic("Failed to initialize VFS!");
+
     while(1)
         cpu_suspend();
 
