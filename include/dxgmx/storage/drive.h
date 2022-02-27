eb1b13395283463a4bb351fc63fd9ac61644b3cc
oda404 <xdxalexandru404@gmail.com>
2022-02-27 15:31:43 +0200

    [storage/drive.h] Add GenericDrive struct.
    Just so we have an early drive interface for the VFS.

--- include/dxgmx/storage/drive.h
+++ include/dxgmx/storage/drive.h
@@ -8,7 +8,34 @@
 
 #include<dxgmx/types.h>
 
-typedef bool(*storage_drive_read)(u64 start, size_t bytes, u8* buf, const void *dev);
-typedef bool(*storage_drive_write)(u64 start, size_t byLtes, const u8 *buf, const void *dev);
+typedef bool(*storage_drive_read)(u64 start, size_t n, u8* buf, const void *dev);
+typedef bool(*storage_drive_write)(u64 start, size_t n, const u8 *buf, const void *dev);
+
+struct S_GenericDrive;
+
+typedef struct
+S_GenericDrivePartition
+{
+    
+} GenericDrivePartition;
+
+typedef struct
+S_GenericDrive
+{
+    const char* const name;
+    const size_t namelen;
+
+    char *mountpoint;
+    size_t mountpoint_len;
+
+    GenericDrivePartition *partitions;
+    size_t partitions_count;
+
+    void* const internal_dev;
+    const storage_drive_read read;
+    const storage_drive_write write;
+
+    const size_t sectorsize;
+} GenericDrive;
 
 #endif // !_DXGMX_STORAGE_DRIVE_H
