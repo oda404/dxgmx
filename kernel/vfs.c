e1862ddda07fff290f7891224414ad3c478ff84e
oda404 <xdxalexandru404@gmail.com>
2022-02-27 17:41:16 +0200

    [vfs] Add 'g_' prefix to global variables.
    I forgor 💀

--- kernel/vfs.c
+++ kernel/vfs.c
@@ -7,19 +7,19 @@
 
 #define KLOGF(lvl, fmt, ...) klogln(lvl, "vfs: " fmt, ##__VA_ARGS__)
 
-static GenericDrive *drives = NULL;
-static size_t drive_count = 0;
+static GenericDrive *g_drives = NULL;
+static size_t g_drives_count = 0;
 
 bool vfs_add_drive(const GenericDrive *d)
 {
     if(!d)
         return false;
 
-    drives = krealloc(drives, (++drive_count) * sizeof(GenericDrive));
-    if(!drives)
+    g_drives = krealloc(g_drives, (++g_drives_count) * sizeof(GenericDrive));
+    if(!g_drives)
         panic("Falied to allocate VFS device!");
 
-    GenericDrive *drive = &drives[drive_count - 1];
+    GenericDrive *drive = &g_drives[g_drives_count - 1];
     /* We memcpy since there are some const members that we can't assign. */
     memcpy(drive, d, sizeof(GenericDrive));
 
