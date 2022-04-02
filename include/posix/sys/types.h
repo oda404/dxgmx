bfe539ea0f8769581da20a0a07599ef8f0a73be3
oda404 <xdxalexandru404@gmail.com>
2022-04-02 23:38:14 +0300

    [posix-types] Add some more types.

--- include/posix/sys/types.h
+++ include/posix/sys/types.h
@@ -8,6 +8,21 @@
 
 #include <stdint.h>
 
-typedef uint32_t mode_t;
+typedef uint16_t mode_t;
+
+typedef uint32_t dev_t;
+typedef uint64_t ino_t;
+typedef uint32_t nlink_t;
+
+typedef uint32_t uid_t;
+typedef uint32_t gid_t;
+
+typedef int64_t off_t;
+
+typedef uint32_t blksize_t;
+typedef uint32_t blkcnt_t;
+
+typedef int64_t time_t;
+typedef int64_t clock_t;
 
 #endif // !_SYS_TYPES_H
