1387e17fc0dd6ab8aa9bc9c1edced83650e2743a
oda404 <xdxalexandru404@gmail.com>
2022-04-05 02:16:37 +0300

    [limits.h] Add limits.h.
    For now, it only defines PATH_MAX to be 512.

---
+++ include/dxgmx/limits.h
@@ -0,0 +1,12 @@
+/**
+ * Copyright 2021 Alexandru Olaru.
+ * Distributed under the MIT license.
+ */
+
+#ifndef _DXGMX_LIMITS_H
+#define _DXGMX_LIMITS_H
+
+/* Including the NULL terminator. */
+#define PATH_MAX 512
+
+#endif // !_DXGMX_LIMITS_H
