a863b6a4709122c91b7f96c563e3ce1d2d555aa8
oda404 <xdxalexandru404@gmail.com>
2022-07-02 22:00:51 +0300

    [lib] Add an empty C library.
    Produces a minimal crt0.o and an empty libc.a just for compiling a
    simple program with the new toolchain.

---
+++ lib/libc/crt0.c
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2022 Alexandru Olaru.
+ * Distributed under the MIT license.
+ */
+
+extern int main(int argc, const char** argv);
+
+int _start()
+{
+    return main(0, (void*)0);
+}
