7191c5f467cec6c6b2291de7c4fc93b3509ad840
oda404 <xdxalexandru404@gmail.com>
2022-09-30 23:52:49 +0300

    [kmalloc] Rewrite kmalloc.
    Transform kmalloc into more of a front-end for whatever driver we
    are using. Right now, the only driver available is "gallocator", which is
    hardcoded in kmalloc_init(). I think it would be a good ideea to get rid
    of this hardcode and implement multi-level modules (something similar to
    linux maybe), that get initialized at different key-points in the kernel
    boot process.
    
    This commit does not compile!

--- include/dxgmx/kmalloc.h
+++ include/dxgmx/kmalloc.h
@@ -6,16 +6,135 @@
 #ifndef _DXGMX_KMALLOC_H
 #define _DXGMX_KMALLOC_H
 
+#include <dxgmx/compiler_attrs.h>
+#include <dxgmx/mem/heap.h>
 #include <dxgmx/types.h>
 
-bool kmalloc_init();
+/**
+ * Kmalloc implementation details.
+ * All allocations made by a driver shall be inside the heap they are passed.
+ * Note how all function pointers that have something to do with memory
+ * allocation/freeing/info take in a 'heap'. When an 'addr' is given alongside a
+ * 'heap', 'addr' is guaranteed to be inside the memory range represented by
+ * said 'heap'. All 'heap' objects and 'addr' pointers passed to any functions
+ * are guaranteed to be non-null.
+ */
+typedef struct S_KMallocDriver
+{
+    /* Name of this driver */
+    char* name;
+
+    /* Priority of this driver over other drivers. */
+    size_t priority;
+
+    /* Initialize this driver. */
+    int (*init)();
+
+    /* Initialize 'heap' to be used with this driver. */
+    int (*init_heap)(const Heap* heap);
+
+    /** Allocate 'size' bytes aligned on 'alignment' bytes.
+     * 'size' is guaranteed to be > 0.
+     * 'alignment' is guaranteed to be a power of two.
+     */
+    void* (*alloc_aligned)(size_t size, size_t alignment, const Heap* heap);
+
+    /**
+     * Reallocate a previous allocation, to be of (at least) 'newsize' bytes.
+     *
+     * 'addr' needs to mentain it's original alignment (the one that was used in
+     * the alloc_aligned() call).
+     *
+     * If the reallocation fails, the original one needs to remain valid, and
+     * the function shall return NULL.
+     *
+     * If the reallocation changes it's address, the new address will be
+     * returned, and the memory copied accordingly from the original allocation.
+     *
+     * 'newsize' is guaranteed to be > 0.
+     *
+     * Returns an address on success, NULL on failure.
+     */
+    void* (*realloc)(void* addr, size_t newsize, const Heap* heap);
+
+    /** Get the size of an allocation.
+     * Returns negative on error.
+     */
+    ssize_t (*allocation_size)(void* addr, const Heap* heap);
+
+    /** Get the alignment of an allocation.
+     * Returns negative on error.
+     */
+    ssize_t (*allocation_alignment)(void* addr, const Heap* heap);
+
+    /**
+     * Returns true if 'addr' is a valid allocated chunk of memory, 'owned' by
+     * this driver.
+     */
+    bool (*is_valid_allocation)(void* addr, const Heap* heap);
+
+    /** Free an allocated address.
+     */
+    void (*free)(void* addr, const Heap* heap);
+
+    /* Default alignment that should be used when allocating memeory with a
+     * function that doesn't explicitly take in the alignnent, ex: kmalloc().
+     * Must be a power of two.
+     */
+    size_t default_alignment;
+} KMallocDriver;
+
+/**
+ * Initialize kmalloc.
+ * The kernel heap needs to be registered before calling this function.
+ * Returns 0 on success, negative on error.
+ */
+int kmalloc_init();
+
+/* Registering a heap to kmalloc implies giving kmalloc full ownersip of the
+ * memory range represented by said heap. No memory in that range is to be used
+ * outside kmalloc/without the use of pointers returned by kmalloc, as it may be
+ * overwritten/wrongly-interpreted by kmalloc. Two more restrictions are inposed
+ * on a 'kernel heap': there is only 1, and it's starting address needs to be
+ * >= the end of the kernel image. This is done for multiple reasons:
+ *
+ *  1: The kernel will make sure that it's heap is higher half mapped in the
+ * same fashion that the kernel image (and 1st MiB of memory on x86) is. This
+ * basically means that the virtual mapping of everything kernel-related,
+ * starting from the 3GiB + 1Mib (on x86) going up to however big the kernel
+ * heap is, is just a higher half mapping from 1MiB to however big the kernel
+ * heap is.
+ *
+ * 2: If the heap is higher-half mapped, then n contiguous pages in virtual
+ * space are also contiguous in physical space.
+ *
+ * 3: Translations from physical to virtual addresses become trivial (just add
+ * the _kernel_map_offset to the physical address).
+ *
+ * 4: This also helps mentally separate everything kernel related,
+ * as the kernel itself is mapped at a high address, with nothing coming
+ * after it (except it's heap, of course). This can be seen easily, just by
+ * looking at an address and seeing if it's bigger than _kernel_map_offset.
+ */
+int kmalloc_register_kernel_heap(Heap heap);
+
+/* Kernel version of malloc. */
 void* kmalloc(size_t size);
+
+/* Kernel version of calloc. Except this only takes in the size, instead of the
+ * element size and number of elements. */
 void* kcalloc(size_t size);
+
+/* kmalloc but the returned address is guaranteed to be aligned on 'alignment'
+ * bytes. */
 void* kmalloc_aligned(size_t size, size_t alignment);
-void kfree(void* addr);
-/* This function should only be used for allocations made with
-kmalloc(). There is currently no way to reallocate kmalloc_aligned()
-allocations other than kfree() and kmalloc_aligned() by yourself. */
+
+/* Kernel version of realloc. If 'addr' was returned by a call to
+ * kmalloc_aligned(), then new address (if changed) is guaranteed to keep it's
+ * alignemnt. */
 void* krealloc(void* addr, size_t size);
+
+/* Kernel version of free. */
+void kfree(void* addr);
 
 #endif //!_DXGMX_KMALLOC_H
--- kernel/mem/kmalloc.c
+++ kernel/mem/kmalloc.c
@@ -4,101 +4,113 @@
  */
 
 #include <dxgmx/assert.h>
+#include <dxgmx/attrs.h>
+#include <dxgmx/errno.h>
+#include <dxgmx/kimg.h>
 #include <dxgmx/klog.h>
 #include <dxgmx/kmalloc.h>
 #include <dxgmx/math.h>
-#include <dxgmx/mem/kheap.h>
+#include <dxgmx/mem/gallocator.h>
+#include <dxgmx/mem/pagesize.h>
 #include <dxgmx/string.h>
 #include <dxgmx/todo.h>
 #include <dxgmx/utils/bitwise.h>
-#include <dxgmx/utils/hashtable.h>
-
-#if defined(_X86_)
-#define KMALLOC_SLAB_SIZE 32
-#define KMALLOC_SLABS_PER_BLOCK 32
-#elif defined(_X86_64_)
-#define KMALLOC_SLAB_SIZE 64
-#define KMALLOC_SLABS_PER_BLOCK 64
-#endif // defined(_X86_)
 
 #define KLOGF_PREFIX "kmalloc: "
+/* KLOG informations about each kmalloc/free call. Very verbose! */
+#define KMALLOC_VERBOSE 0
 
-#define KMALLOC_BLOCKS_COUNT                                                   \
-    (KERNEL_HEAP_SIZE / KMALLOC_SLABS_PER_BLOCK / KMALLOC_SLAB_SIZE)
+static bool g_kmalloc_up;
+/* The kmalloc driver. */
+static KMallocDriver g_driver;
+/* The kernel heap. */
+static Heap g_kheap;
 
-#define KMALLOC_LOG_ACTIONS 0
-
-static HashTable g_allocation_hashtable;
-static u64 g_allocator_blocks[KMALLOC_BLOCKS_COUNT];
-
-static _ATTR_ALWAYS_INLINE bool is_vaddr_inside_heap(ptr vaddr)
+static int kmalloc_check_driver(const KMallocDriver* drv)
 {
-    return (
-        (ptr)vaddr >= kheap_get_start_vaddr() &&
-        (ptr)vaddr < kheap_get_start_vaddr() + kheap_get_size());
+    /* drv->realloc is optional, because I'm a dipshit. */
+    bool valid =
+        (drv->name && drv->init && drv->init_heap && drv->alloc_aligned &&
+         drv->allocation_size && drv->allocation_alignment &&
+         drv->is_valid_allocation && drv->free);
+
+    valid = valid && bw_is_power_of_two(drv->default_alignment);
+
+    if (!valid)
+        return -EINVAL;
+
+    return 0;
 }
 
-static _ATTR_ALWAYS_INLINE ptr
-vaddr_from_block_and_slab(size_t block, size_t slab_offset)
+/* kmalloc_aligned() that specifically takes in a heap. */
+static void*
+kmalloc_aligned_with_heap(size_t size, size_t alignment, Heap* heap)
 {
-    return kheap_get_start_vaddr() +
-           block * KMALLOC_SLABS_PER_BLOCK * KMALLOC_SLAB_SIZE +
-           slab_offset * KMALLOC_SLAB_SIZE;
+    return g_driver.alloc_aligned(size, alignment, heap);
 }
 
-static _ATTR_ALWAYS_INLINE size_t block_from_vaddr(ptr vaddr)
+/* kfree() that specifically takes in a heap. */
+static void kfree_with_heap(void* addr, Heap* heap)
 {
-    return (vaddr - kheap_get_start_vaddr()) /
-           (KMALLOC_SLABS_PER_BLOCK * KMALLOC_SLAB_SIZE);
+    g_driver.free(addr, heap);
 }
 
-static i32
-find_contiguous_blocks_aligned(size_t start, size_t blocks, size_t alignment)
+int kmalloc_register_kernel_heap(Heap heap)
 {
-    /* Make sure the start block is aligned. */
-    ptr addr = vaddr_from_block_and_slab(start, 0);
-    addr = (addr + alignment - 1) & ~(alignment - 1);
-    start = block_from_vaddr(addr);
+    /* We can only register one kernel heap. NOTE: should we really have this
+     * restriction ? */
+    if (g_kheap.vaddr || g_kheap.pagespan)
+        return -EEXIST;
 
-    if (start >= KMALLOC_BLOCKS_COUNT || !blocks)
-        return -1;
+    /* The kernel heap should start after the kernel image. */
+    if (heap.vaddr < kimg_vaddr() + kimg_size() || heap.pagespan == 0)
+        return -EINVAL;
 
-    size_t block_stepping =
-        ceil((double)alignment / (KMALLOC_SLABS_PER_BLOCK * KMALLOC_SLAB_SIZE));
+    g_kheap = heap;
 
-    for (size_t i = start; i < KMALLOC_BLOCKS_COUNT; i += block_stepping)
-    {
-        size_t found = 0;
-        for (size_t k = 0; k < blocks && i + k < KMALLOC_BLOCKS_COUNT; ++k)
-        {
-            if (g_allocator_blocks[i + k])
-                break;
-            else
-                ++found;
-        }
-
-        if (found == blocks)
-            return i;
-    }
-
-    return -1;
+    return 0;
 }
 
-bool kmalloc_init()
+_INIT int kmalloc_init()
 {
-    memset(g_allocator_blocks, 0, sizeof(g_allocator_blocks));
+    /* For now the gallocator is the only one we have... Optimistically we would
+     * build allocators as modules, but right now we only have a single 'level'
+     * of modules that gets initialized long after kmalloc came online. */
+    g_driver = (KMallocDriver){
+        .name = "gallocator",
+        .priority = 1,
+        .default_alignment = sizeof(ptr),
+        .init = gallocator_init,
+        .init_heap = gallocator_init_heap,
+        .alloc_aligned = gallocator_alloc_aligned,
+        .realloc = NULL, /* Let krealloc do it the 'dumb' way. */
+        .allocation_size = gallocator_allocation_size,
+        .allocation_alignment = gallocator_allocation_alignment,
+        .is_valid_allocation = gallocator_is_valid_allocation,
+        .free = gallocator_free};
 
-    /* FIXME: This creates a circular dependency where
-    the hashtable needs kmalloc to be in working order and
-    kmalloc needs the hashtable to keep track of all allocation sizes. */
-    hashtable_init(512, &g_allocation_hashtable);
+    int st = kmalloc_check_driver(&g_driver);
+    if (st < 0)
+        return st;
 
-    return true;
+    /* Init the driver. */
+    st = g_driver.init();
+    if (st < 0)
+        return st;
+
+    /*  Init the kernel heap. */
+    st = g_driver.init_heap(&g_kheap);
+    if (st < 0)
+        return st;
+
+    g_kmalloc_up = true;
+
+    return 0;
 }
 
 void* kmalloc(size_t size)
 {
-    return kmalloc_aligned(size, 4);
+    return kmalloc_aligned(size, g_driver.default_alignment);
 }
 
 void* kcalloc(size_t size)
@@ -110,170 +122,135 @@ void* kcalloc(size_t size)
     return addr;
 }
 
+/* Small note: all kmalloc functions basically boil down to this one. */
 void* kmalloc_aligned(size_t size, size_t alignment)
 {
-    if (!size || !bw_is_power_of_two(alignment))
+    if (UNLIKELY(!g_kmalloc_up))
         return NULL;
 
-    size_t slabs = ceil((double)size / KMALLOC_SLAB_SIZE);
-    size_t blocks = slabs / KMALLOC_SLABS_PER_BLOCK;
+    if (!size)
+        return NULL;
 
-    u64 slabs_mask = 0;
-    i32 start_block = 0;
+    if (!bw_is_power_of_two(alignment))
+        panic("kmalloc_aligned: alignment is not a power of two!");
 
-    if (blocks)
+    Heap* heap = &g_kheap;
+
+    void* addr = kmalloc_aligned_with_heap(size, alignment, heap);
+
+    /* Sanity check */
+    ASSERT(((ptr)addr % alignment) == 0);
+
+#if KMALLOC_VERBOSE == 1
+    if (addr)
     {
-        start_block = find_contiguous_blocks_aligned(0, blocks, alignment);
-        if (start_block < 0)
-        {
-            KLOGF(WARN, "Failed to find %u contiguous bytes.", size);
-            return NULL;
-        }
-
-        slabs -= blocks * KMALLOC_SLABS_PER_BLOCK;
-        if (!slabs)
-            goto done; /* how nice. */
-
-        /* build a mask for the new 'half' block */
-        for (size_t i = 0; i < slabs; ++i)
-            bw_set(&slabs_mask, i);
-
-        u64 block = g_allocator_blocks[start_block + blocks];
-        while (bw_mask(block, slabs_mask))
-        {
-            /* Try to find a new start_block. If this fails at any point, it
-            means we can't make this allocation as we reached the end of the
-            heap without finding anything. */
-            start_block = find_contiguous_blocks_aligned(
-                start_block + 1, blocks, alignment);
-            if (start_block < 0)
-            {
-                KLOGF(WARN, "Failed to find %u contiguous bytes.", size);
-                return NULL;
-            }
-            block = g_allocator_blocks[start_block + blocks];
-        }
+        KLOGF(
+            DEBUG,
+            "Allocated 0x%X bytes at 0x%p (alignment 0x%X)",
+            size,
+            addr,
+            alignment);
     }
     else
     {
-        for (size_t i = 0; i < KMALLOC_BLOCKS_COUNT; ++i)
-        {
-            start_block = i;
-            u64 block = g_allocator_blocks[i];
-
-            slabs_mask = 0;
-            for (size_t k = 0; k < slabs; ++k)
-                bw_set(&slabs_mask, k);
-
-            size_t k = 0;
-            for (; k < KMALLOC_SLABS_PER_BLOCK - slabs; ++k)
-            {
-                if (!bw_mask(block, slabs_mask))
-                    goto done;
-                slabs_mask <<= 1;
-            }
-        }
-
-        KLOGF(WARN, "Failed to find %u contiguous bytes.", size);
-        return NULL;
+        KLOGF(
+            DEBUG,
+            "Failed to allocate 0x%X bytes (alignment 0x%X)",
+            size,
+            alignment);
     }
+#endif
 
-done:
-    /* mark any whole blocks. */
-    for (size_t i = 0; i < blocks; ++i)
-        bw_or_mask(&g_allocator_blocks[start_block + i], 0xFFFF'FFFF'FFFF'FFFF);
-    /* apply slabs_mask if  */
-    if (slabs_mask)
-        bw_or_mask(&g_allocator_blocks[start_block + blocks], slabs_mask);
-
-    size_t start_slab = 0;
-    if (slabs_mask)
-    {
-        while (!((slabs_mask >> start_slab) & 1))
-            ++start_slab;
-    }
-
-    const ptr addr = vaddr_from_block_and_slab(start_block, start_slab);
-
-    if (g_allocation_hashtable.entries_max)
-    {
-        // FIXME: Remove this shit.
-        if (!hashtable_add(addr, (void*)size, &g_allocation_hashtable))
-            panic("kmalloc: Allocation hashtable reached it's capacity.");
-    }
-
-#if KMALLOC_LOG_ACTIONS
-    KLOGF(
-        DEBUG,
-        "Allocated %u(%u) bytes at 0x%p.",
-        size,
-        KMALLOC_SLAB_SIZE * slabs +
-            KMALLOC_SLAB_SIZE * KMALLOC_SLABS_PER_BLOCK * blocks,
-        (void*)addr);
-#endif // KMALLOC_LOG_ACTIONS
-
-    return (void*)addr;
+    return addr;
 }
 
 void kfree(void* addr)
 {
-    if ((ptr)addr % KMALLOC_SLAB_SIZE || !is_vaddr_inside_heap((ptr)addr))
-        panic("Tried to kfree() an invalid address!");
+    if (UNLIKELY(!g_kmalloc_up))
+        return;
 
-    const size_t size =
-        (size_t)hashtable_get((size_t)addr, &g_allocation_hashtable);
-    if (!size)
-        panic("Tried to kfree() an invalid address!");
+    if (!addr)
+        panic("kfree: Tried to kfree a NULL address!");
 
-    size_t slabs = ceil((double)size / KMALLOC_SLAB_SIZE);
-    size_t blocks = slabs / KMALLOC_SLABS_PER_BLOCK;
-    slabs -= blocks * KMALLOC_SLABS_PER_BLOCK;
+    Heap* heap = &g_kheap;
+    if (!heap_is_addr_inside((ptr)addr, heap))
+        panic("kfree: Tried to free an invalid address (0x%p)!", addr);
 
-    size_t start_block = block_from_vaddr((ptr)addr);
-    for (size_t i = 0; i < blocks; ++i)
-        g_allocator_blocks[start_block + i] = 0;
+    kfree_with_heap(addr, heap);
 
-    if (slabs)
-    {
-        /* build the bitmask for the slabs */
-        size_t offset =
-            ((ptr)addr % (KMALLOC_SLAB_SIZE * KMALLOC_SLABS_PER_BLOCK)) /
-            KMALLOC_SLAB_SIZE;
-        u64 mask = 0xFFFF'FFFF'FFFF'FFFF;
-        for (size_t i = 0; i < slabs; ++i)
-            bw_clear(&mask, i + offset);
-
-        bw_and_mask(&g_allocator_blocks[start_block + blocks], mask);
-    }
-
-    hashtable_remove((size_t)addr, &g_allocation_hashtable);
-
-#if KMALLOC_LOG_ACTIONS
-    KLOGF(
-        DEBUG,
-        "Freed %u(%u) bytes at 0x%p.",
-        size,
-        KMALLOC_SLAB_SIZE * slabs +
-            KMALLOC_SLAB_SIZE * KMALLOC_SLABS_PER_BLOCK * blocks,
-        addr);
-#endif // KMALLOC_LOG_ACTIONS
+#if KMALLOC_VERBOSE == 1
+    KLOGF(DEBUG, "Freed address 0x%p", addr);
+#endif
 }
 
 void* krealloc(void* addr, size_t size)
 {
-    void* newaddr = NULL;
+    if (UNLIKELY(!g_kmalloc_up))
+        return NULL;
+
+    if (!size)
+        return NULL;
+
+    Heap* heap = &g_kheap;
+    if (addr && !heap_is_addr_inside((ptr)addr, heap))
+        return NULL;
+
     if (addr)
     {
-        const size_t oldsize =
-            (size_t)hashtable_get((size_t)addr, &g_allocation_hashtable);
-        kfree(addr);
-        newaddr = kmalloc(size);
-        memcpy(newaddr, addr, oldsize);
-    }
-    else
-    {
-        newaddr = kmalloc(size);
+        /* If the driver implements 'realloc', we let it do it's thing. */
+        if (g_driver.realloc)
+            return g_driver.realloc(addr, size, heap);
+
+        /* If not, we do it outselvers, but of course, since we don't know how
+         * the driver works, it's gonna be a less-than-optimal solution. */
+        size_t prevsize;
+        size_t alignment;
+
+        /* Check and set size. */
+        ssize_t res = g_driver.allocation_size(addr, heap);
+        if (res <= 0)
+        {
+            KLOGF(ERR, "%s: allocation_size() returned %d", g_driver.name, res);
+            return NULL;
+        }
+        prevsize = res;
+
+        /* Simple enough */
+        if (prevsize == size)
+            return addr;
+
+        /* Check and set alignment. */
+        res = g_driver.allocation_alignment(addr, heap);
+        if (res < 0)
+        {
+            KLOGF(
+                ERR,
+                "%s: allocation_alignment() returned %d",
+                g_driver.name,
+                res);
+            return NULL;
+        }
+        alignment = res;
+        if (!bw_is_power_of_two(alignment))
+        {
+            panic(
+                "krealloc: %s: returned non-power of two alignment!",
+                g_driver.name);
+        }
+
+        /* Allocate a new chunk of memory, copy the old memory into the new one,
+         * and free the old one. We don't free the old allocation first, to make
+         * sure that if the new allocation fails, the old one is still valid, as
+         * per spec. */
+        void* ret = kmalloc_aligned_with_heap(size, alignment, heap);
+        if (!ret)
+            return NULL;
+
+        memcpy(ret, addr, (prevsize < size) ? prevsize : size);
+        kfree_with_heap(addr, heap);
+        return ret;
     }
 
-    return newaddr;
+    /* addr is null, so we treat this lile a simple kmalloc() call. */
+    return kmalloc_aligned_with_heap(size, g_driver.default_alignment, heap);
 }
