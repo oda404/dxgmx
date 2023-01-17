4d6cea62c109a57e4beb36c44de276a34222e5b8
oda404 <xdxalexandru404@gmail.com>
2023-01-17 17:00:10 +0200

    userspace: Mark the userspace family of functions with NORETURN.

--- include/dxgmx/userspace.h
+++ include/dxgmx/userspace.h
@@ -6,9 +6,10 @@
 #ifndef _DXGMX_USERSPACE_H
 #define _DXGMX_USERSPACE_H
 
+#include <dxgmx/compiler_attrs.h>
 #include <dxgmx/types.h>
 
-void userspace_jump2user(ptr instrptr, ptr stackptr);
-void userspace_arch_jump2user(ptr instrptr, ptr stackptr);
+_ATTR_NORETURN void userspace_jump2user(ptr instrptr, ptr stackptr);
+_ATTR_NORETURN void userspace_arch_jump2user(ptr instrptr, ptr stackptr);
 
 #endif // !_DXGMX_USERSPACE_H
