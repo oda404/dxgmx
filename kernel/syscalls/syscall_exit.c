692e1db3d2b5798c8f9ad63d2d7bbe40293d9e38
oda404 <xdxalexandru404@gmail.com>
2023-01-17 17:17:11 +0200

    syscall_exit: Kill the process using the new sched and procm API.

--- kernel/syscalls/syscall_exit.c
+++ kernel/syscalls/syscall_exit.c
@@ -4,11 +4,18 @@
  */
 
 #include <dxgmx/klog.h>
+#include <dxgmx/proc/procmanager.h>
+#include <dxgmx/sched/scheduler.h>
 #include <dxgmx/syscalls.h>
 #include <dxgmx/todo.h>
 
 void syscall_exit(int status)
 {
-    klogln(DEBUG, "exit(%d)", status);
-    TODO_FATAL();
+    /* We can't straight up kill and free the curernt process, since that would
+     * mean freeing the process' kernel stack, which is also the stack we are
+     * using right now. We deal with this by marking it as dead, and letting the
+     * scheduler clean it up when it knows it safe to do so. */
+    procm_mark_dead(status, sched_current_proc());
+
+    sched_yield();
 }
