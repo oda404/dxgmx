e3823eb7f646a791cbafb41f31ac8d53f5bed3d9
oda404 <xdxalexandru404@gmail.com>
2023-01-17 17:14:54 +0200

    kinit_stage3: Call exit syscall.

--- kernel/kinit_stage3.c
+++ kernel/kinit_stage3.c
@@ -1,5 +1,9 @@
+/**
+ * Copyright 2022 Alexandru Olaru.
+ * Distributed under the MIT license.
+ */
 
-
+#include <dxgmx/assert.h>
 #include <dxgmx/compiler_attrs.h>
 #include <dxgmx/syscalls.h>
 
@@ -8,14 +12,19 @@
  * I remember reading somewhere that linux does something similar, and it
  * seems like a really elegant solution on how to spawn the init process without
  * the kernel reading the binary off of disk using non-syscalls methods.
- * This way we spawn this binary as pid 1 and then, let it spawn the actual 
- * pid 1 using the exec syscall. If you're curious about how this image is 
+ * This way we spawn this binary as pid 1 and then, let it spawn the actual
+ * pid 1 using the exec syscall. If you're curious about how this image is
  * loaded in memory check procm_spawn_init().
  */
 
-_ATTR_SECTION(".kinit_stage3_text") int kinit_stage3_main()
+_ATTR_SECTION(".kinit_stage3_text") _ATTR_NORETURN int kinit_stage3_main()
 {
-    while (1)
-        ;
-    return 0;
+
+    /* exit(0) syscall */
+    __asm__ volatile("int $0x80"
+                     :
+                     : "a"(SYSCALL_EXIT), "b"(0)
+                     : "memory", "cc", "ecx", "edx", "esi", "edi");
+
+    ASSERT_NOT_HIT();
 }
