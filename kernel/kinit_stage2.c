db5f5b00ef42da2eeef97679bba68e2bf5ec1cfe
oda404 <xdxalexandru404@gmail.com>
2023-01-17 17:13:20 +0200

    kinit_stage2: Spawn PID 1 and call scheduler.

--- kernel/kinit_stage2.c
+++ kernel/kinit_stage2.c
@@ -4,25 +4,30 @@
  */
 
 #include <dxgmx/attrs.h>
-#include <dxgmx/cpu.h>
 #include <dxgmx/fs/vfs.h>
 #include <dxgmx/module.h>
 #include <dxgmx/panic.h>
+#include <dxgmx/proc/procmanager.h>
+#include <dxgmx/sched/scheduler.h>
 #include <dxgmx/syscalls.h>
 #include <dxgmx/types.h>
 
-bool kinit_stage2()
+_ATTR_NORETURN bool kinit_stage2()
 {
     if (syscalls_init() < 0)
         panic("Failed to setup system calls. Not proceeding!");
 
     module_init_builtins();
 
-    if (!vfs_init())
+    if (vfs_init() < 0)
         panic("Failed to initialize VFS!");
 
-    while (1)
-        cpu_suspend();
+    /* Spawn PID 1. Here, we only load the binary in memory and have it ready to
+     * run, waiting for the scheduler to kick in. For details on how this binary
+     * works check out kernel/kinit_stage3.c */
+    if (procm_spawn_init() != 1)
+        panic("Failed to spawn PID 1!");
 
-    return true;
+    /* Let it rip */
+    sched_init();
 }
