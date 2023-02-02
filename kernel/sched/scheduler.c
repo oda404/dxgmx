051214010a0dd90817857a38a4742ba3aaef1cf3
oda404 <xdxalexandru404@gmail.com>
2023-02-03 01:31:10 +0200

    scheduler: Update scheduler to make use of the new procm API.

--- kernel/sched/scheduler.c
+++ kernel/sched/scheduler.c
@@ -1,17 +1,12 @@
 /**
- * Copyright 2022 Alexandru Olaru.
+ * Copyright 2023 Alexandru Olaru.
  * Distributed under the MIT license.
  */
 
 #include <dxgmx/klog.h>
-#include <dxgmx/mem/mm.h>
 #include <dxgmx/panic.h>
 #include <dxgmx/proc/procmanager.h>
 #include <dxgmx/sched/scheduler.h>
-#include <dxgmx/syscalls.h>
-#include <dxgmx/time.h>
-#include <dxgmx/todo.h>
-#include <dxgmx/userspace.h>
 
 #define KLOGF_PREFIX "sched: "
 
@@ -24,45 +19,27 @@ Process* sched_current_proc()
 
 void sched_init()
 {
-    Process* procs = procm_procs();
     size_t proc_count = procm_proc_count();
 
     /* We should have 1 process, kinit_stage3 */
-    if (!procs || proc_count != 1)
-        panic("Scheduler fount no PID 1!");
+    if (proc_count != 1)
+        panic("Scheduler found no PID 1!");
 
-    g_current_proc = &procs[0];
+    KLOGF(INFO, "Starting PID 1.");
+    g_current_proc = procm_next_queued_proc();
     procm_switch_ctx(g_current_proc);
 }
 
 void sched_yield()
 {
-    Process* procs = procm_procs();
-    size_t proc_count = procm_proc_count();
+    Process* proc = procm_next_queued_proc();
 
-    FOR_EACH_ELEM_IN_DARR (procs, proc_count, proc)
+    while (proc->dead)
     {
-        if (proc->dead)
-        {
-            if (proc->pid == g_current_proc->pid)
-            {
-                /* We have one process and it's trying to die, I.E. pid 1 has
-                 * returned */
-                if (proc_count == 1)
-                    panic(
-                        "PID 1 returned %d, no more work to do.",
-                        proc->exit_status);
-
-                /* If the currenct process is trying to die, we skip it for now,
-                since freeing it would mean freeing the kernel stack we are
-                currently using */
-                continue;
-            }
-
-            procm_kill(proc);
-            continue;
-        }
+        procm_try_kill_proc(g_current_proc, proc);
+        proc = procm_next_queued_proc();
     }
 
-    TODO_FATAL();
+    g_current_proc = proc;
+    procm_switch_ctx(g_current_proc);
 }
