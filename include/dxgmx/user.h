03442c02b8714c97482b9918a240e4817fa91565
oda404 <xdxalexandru404@gmail.com>
2023-08-05 23:11:33 +0300

    user-x86: Add user_copy_str_from().

--- include/dxgmx/user.h
+++ include/dxgmx/user.h
@@ -6,10 +6,16 @@
 #ifndef _DXGMX_USERSPACE_H
 #define _DXGMX_USERSPACE_H
 
+#include <dxgmx/attrs.h>
 #include <dxgmx/compiler_attrs.h>
 #include <dxgmx/types.h>
 
-_CDECL _ATTR_NEVER_INLINE int user_copy_to(void* dest, void* src, size_t n);
+_CDECL _ATTR_NEVER_INLINE int
+user_copy_to(_USERPTR void* dest, const void* src, size_t n);
+
+_CDECL _ATTR_NEVER_INLINE int
+user_copy_str_from(_USERPTR const void* src, void* dest, size_t maxn);
+
 int user_access_fault_stub();
 
 _ATTR_NORETURN void user_jump2user(ptr instrptr, ptr stackptr);
