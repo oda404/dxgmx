2ea944803add03ac8e74a1aead6d7decccb4d93c
oda404 <xdxalexandru404@gmail.com>
2023-08-16 22:18:58 +0300

    bake_symbols: Only embed T or t symbols.

--- tools/bake_symbols.sh
+++ tools/bake_symbols.sh
@@ -2,16 +2,15 @@
 
 KERNEL_BIN=$1
 
-final=$(mktemp)
 tmp=$(mktemp)
+final=build/$KERNEL_BIN.map
 
-$NM -n $KERNEL_BIN > $tmp
+$NM -n $KERNEL_BIN | awk '{ if($2 == "T" || $2 == "t") printf("%s %s %s\n", $1, $2, $3); }' > $tmp
+
 printf "1BADADD4\n" > $final
 printf "%08X\n" $(wc -l < $tmp) >> $final
 cat $tmp >> $final
 
 $OBJCOPY --update-section .ksyms=$final $KERNEL_BIN
-cp $final build/$KERNEL_BIN.map
 
 rm -rf $tmp
-rm -rf $final
